f0a4781c9022a95c154fac9c0b6186e8f1d591f7
Eduardo Alvarez <eduardo.a1993@gmail.com>
2019-10-02 01:00:14 -0300

    Agregar dependnecias necesarias para levantar el proyecto en local

---
+++ src/main.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const Main = () => <h1>Hello Spec Atelier</h1>;
+
+ReactDOM.render(<Main />, document.querySelector('#specAtelier'));
--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,44 @@
-module.exports = {
-	module: {
-		rules: [
-			{
-				test: /\.(js|jsx)$/,
-				exclude: /node_modules/,
-				use: {
-					loader: 'babel-loader',
+const webpack = require('webpack');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = env => {
+	return {
+		entry: path.resolve(__dirname, 'src', 'main.js'),
+		output: {
+			path: path.resolve(__dirname, 'dist'),
+			filename: 'bundle.js',
+		},
+		devServer: {
+			contentBase: './',
+			port: 8080,
+			compress: true,
+			inline: true,
+			historyApiFallback: true,
+		},
+		resolve: {
+			extensions: ['.jsx', '.js'],
+		},
+		module: {
+			rules: [
+				{
+					test: /\.(js|jsx)$/,
+					exclude: /node_modules/,
+					use: {
+						loader: 'babel-loader',
+					},
 				},
-			},
+			],
+		},
+		externals: ['window'],
+		plugins: [
+			new webpack.ProgressPlugin(),
+			new HtmlWebPackPlugin({
+				template: './src/index.html',
+			}),
+			new webpack.DefinePlugin({
+				ENVIRONMENT: JSON.stringify(env.NODE_ENV),
+			}),
 		],
-	},
-}
+	};
+};
