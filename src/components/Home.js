05576643dd8883df1bde19cde797974ab5f56caa
Sophia-Sergio <san.storres@gmail.com>
2019-10-07 22:54:54 -0300

    Add sign_up feature

---
+++ src/components/Home.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import Registration from './auth/Registration';
+
+export default class Home extends Component {
+
+  handleSuccessfullAuth = data => {
+    this.props.handleLogin(data);
+    console.log('hola dashboard');
+    this.props.history.push("/dashboard")
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Home</h1>
+        <h1>Status: {this.props.loggedInStatus}</h1>
+       <Registration handleSuccessfullAuth={this.handleSuccessfullAuth}/>
+      </div>
+    )
+  }
+}
---
+++ src/components/auth/Registration.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+export default class Registration extends Component {
+
+  state = {
+    email: '',
+    password: '',
+    password_confirmation: '',
+    registrationErrors: null
+  }
+
+  handleSubmit = event => {
+    event.preventDefault();
+    const { email, password } = this.state;
+    axios.post(process.env.API_URL + '/registrations',
+     { user: { email: email, password: password} },
+     { withCredentials: true }
+    ).then(response => {
+      if (response.data.status === 'created'){
+        this.props.handleSuccessfullAuth(response.data)
+      }
+    }).catch(error => {
+      this.setState({ registrationErrors: error })
+    })
+  };
+
+  handleChange = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+  }
+
+  render() {
+    return(
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          {this.state.registrationErrors}
+          <input type="email" name='email' value={this.state.email} onChange={this.handleChange} required/>
+          <input type="password" name='password' value={this.state.password} onChange={this.handleChange} required/>
+          <button type='submit'>Register</button>
+        </form>
+      </div>
+    )
+  }
+}
