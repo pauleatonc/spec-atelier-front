d0fb60a34580d343b687d20c95ceb1d4d6ab1e38
Eduardo Alvarez <eduardo.a1993@gmail.com>
2019-10-10 14:36:31 -0300

    Refactor in config file

--- src/config/config.js
+++ src/config/config.js
@@ -1,15 +1,14 @@
-export default {
-	handleGetUrl() {
-		const url = {
-			development: 'http://localhost:3000',
-			production: 'https://spec-atelier.herokuapp.com',
-		};
-		return url[this.handleGetEnvironment()];
-	},
-	handleGetEnvironment() {
-		return ENVIRONMENT;
-	},
-	handleGetEntryPointApi(path) {
-		return `/api/${path}`;
-	},
+/* eslint-disable no-undef */
+const mapUrls = {
+	development: 'http://localhost:3000',
+	production: 'https://spec-atelier.herokuapp.com',
 };
+
+const handleGetEnvironment = () => ENVIRONMENT;
+const handleGetUrl = () => mapUrls[handleGetEnvironment()];
+const handleGetEntryPointApi = path => `/api/${path}`;
+
+const getEnpoint = (path = '') =>
+	`${handleGetUrl()}${handleGetEntryPointApi(path)}`;
+
+export default getEnpoint;
