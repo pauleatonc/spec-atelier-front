0f10230e5d75dcecf38533c3c9ffc3b7973061ba
Eduardo Alvarez <eduardo.a1993@gmail.com>
2019-10-17 15:15:29 -0300

    Create functions to manipulate localstorage api

---
+++ src/helpers/localstorage.helper.js
@@ -0,0 +1,17 @@
+const setLocalStorage = (key, value) => (
+    localStorage.setItem(key, value)
+);
+
+const getLocalStorage = key => (
+    localStorage.getItem(key)
+);
+
+const deleteLocalStorage = key => (
+    localStorage.removeItem(key)
+)
+
+export default {
+    setLocalStorage,
+    getLocalStorage,
+    deleteLocalStorage
+};
