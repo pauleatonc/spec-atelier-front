09916abc9c33d87eca37a3018f9b38a02236fba8
Eduardo Alvarez <eduardo.a1993@gmail.com>
2019-10-22 23:16:29 -0300

    Create unit test for all teh functions of the localstorage helper

--- __test__/helpers/localstorage.spec.js
+++ __test__/helpers/localstorage.spec.js
@@ -1,16 +1,43 @@
 /* eslint-disable no-undef */
 import 'jest-localstorage-mock';
-import { setLocalStorage } from '../../src/helpers/localstorage.helper';
+import {
+	setLocalStorage,
+	getLocalStorage,
+	deleteLocalStorage,
+} from '../../src/helpers/localstorage.helper';
 
 describe('LocalStorage Helper', () => {
-	it('should save new key to localStorage', () => {
-		const key = 'foo';
-		const value = 'bar';
-		const valueStringify = JSON.stringify(value);
+	let key;
+	let value;
+	let valueStringify;
 
+	beforeAll(() => {
+		key = 'foo';
+		value = 'bar';
+		valueStringify = JSON.stringify(value);
+	});
+
+	afterAll(() => {
+		localStorage.clear();
+		localStorage.__STORE__ = {};
+	});
+
+	it('should save new key to localStorage', () => {
 		setLocalStorage({ key, value });
 		expect(localStorage.setItem).toHaveBeenLastCalledWith(key, valueStringify);
 		expect(localStorage.__STORE__[key]).toBe(valueStringify);
 		expect(Object.keys(localStorage.__STORE__).length).toBe(1);
+	});
+
+	it('should get value from localsotrage', () => {
+		setLocalStorage({ key, value });
+		expect(getLocalStorage(key)).toBe(value);
+		expect(Object.keys(localStorage.__STORE__).length).toBe(1);
+	});
+
+	it('should delete key', () => {
+		setLocalStorage({ key, value });
+		expect(deleteLocalStorage(key)).toBe(undefined);
+		expect(Object.keys(localStorage.__STORE__).length).toBe(0);
 	});
 });
