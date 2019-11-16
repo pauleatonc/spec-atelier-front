ae5cd6bbe5f8dec3ee5892eb49ce23f7f6c47a22
Eduardo Alvarez <eduardo.a1993@gmail.com>
2019-11-16 17:07:55 -0300

    Create new section - Why be a supplier

---
+++ src/components/home/why-be-supplier/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const WhyBeSupplier = () => (
+	<section className="supplier">
+		<div className="supplier__inner">
+			<h1 className="supplier__inner__title">¿por qué ser proveedor?</h1>
+			<p className="supplier__inner__description">
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+				veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+				commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+				velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+				occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+				mollit anim id est laborum.h
+			</p>
+			<div className="supplier__inner__bottom">
+				<button type="button" className="supplier__inner__bottom__btn">
+					Únete
+				</button>
+			</div>
+		</div>
+	</section>
+);
+
+export default WhyBeSupplier;
