34dc431f69eff9ee9fffa90921dd69fa04e3784f
Eduardo Alvarez <eduardo.a1993@gmail.com>
2019-11-18 02:02:05 -0300

    Change format for moment

--- src/helpers/pretty-format.helper.js
+++ src/helpers/pretty-format.helper.js
@@ -3,12 +3,12 @@ import Moment from 'react-moment';
 import 'moment/locale/es';
 
 export const firstLetterToUppercase = string =>
-	string.charAt(0).toUpperCase() + string.slice(1);
+	`${string.charAt(0).toUpperCase()}${string.slice(1)}`;
 
 export const handleChangeToPrettyFormat = date => (
-	<Moment local="es" date={date} format="DD/MM/YYYY" />
+	<Moment local="es" date={date} format="LL" />
 );
 
 export const handleChangeToprettyFormatToNow = date => (
-	<Moment local="es" date={date} fromNow />
+	<Moment local="es" date={date} fromNow interval={30000} />
 );
