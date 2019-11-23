aed1ac8d28f59016d5e3f3fe22c93a19963c7179
Eduardo Alvarez <eduardo.a1993@gmail.com>
2019-11-22 22:25:58 -0300

    Add styles to footer

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -1,9 +1,58 @@
 import React from 'react';
 
+import { Link } from 'react-router-dom';
+
 const Footer = () => (
-	<section className="footer">
-		<h1>Footer</h1>
-	</section>
+	<footer className="footer">
+		<div className="footer__inner">
+			<div className="footer__inner__info">
+				<div className="footer__inner__info__description">
+					<div className="footer__inner__info__description__inner">
+						<h2 className="footer__inner__info__description__inner__title">
+							Contáctanos
+						</h2>
+						<div className="footer__inner__info__description__inner__content">
+							<p className="footer__inner__info__description__inner__content__data">
+								contacto@specatelier.com
+							</p>
+							<p className="footer__inner__info__description__inner__content__data">
+								+56 9 8765 4321
+							</p>
+						</div>
+					</div>
+
+					<div className="footer__inner__info__description__inner">
+						<div className="footer__inner__info__description__inner__content">
+							<p className="footer__inner__info__description__inner__content__data">
+								Copyright &copy; 2019 Spec Atelier. Todos los derechos
+								reservados.
+							</p>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div className="footer__inner__social">
+				<div className="footer__inner__social__menu">
+					<Link className="footer__inner__social__menu__link" to="/">
+						Inicio
+					</Link>
+					<Link className="footer__inner__social__menu__link" to="/products">
+						Productos
+					</Link>
+					<Link className="footer__inner__social__menu__link" to="/brands">
+						Marcas
+					</Link>
+					<Link className="footer__inner__social__menu__link" to="/us">
+						Nosotros
+					</Link>
+				</div>
+
+				<div className="footer__inner__social__networking">
+					Facebook Twitter Instagram
+				</div>
+			</div>
+		</div>
+	</footer>
 );
 
 export default Footer;
