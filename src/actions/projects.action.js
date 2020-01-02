4899ae9926869abcb4e5ef61a13736611f99bd8b
Sophia-Sergio <san.storres@gmail.com>
2020-01-02 17:59:13 -0300

    refactor

--- src/actions/projects.action.js
+++ src/actions/projects.action.js
@@ -2,7 +2,6 @@
 /* eslint-disable no-undef */
 /* eslint-disable no-console */
 /* eslint-disable import/no-unresolved */
-import 'babel-polyfill';
 import getEndPoint from '@Configurations/config';
 import { getLocalStorage } from '@Helpers/localstorage.helper';
 import { GET_ALL_PROJECTS, GET_ORDERED_PROJECTS } from '@Configurations/constants';
@@ -12,21 +11,21 @@ const userID = getLocalStorage('userID');
 const token = getLocalStorage('token');
 const apiHost = `${endpoint}/${userID}/projects`;
 const toQueryString = obj => `?${Object.keys(obj).map(key => `${key}=${obj[key]}`).join('&')}`;
-const dispatchFormat = (type, payload) => ({type, payload});
+const dispatchFormat = (type, payload) => ({ type, payload });
 
 export const getAllProjectsAction = dispatch => _ => {
-  request().then(result => dispatch(
-    dispatchFormat(GET_ALL_PROJECTS, {projects: result.projects, loader: false})
+  request({}).then(result => dispatch(
+    dispatchFormat(GET_ALL_PROJECTS, { projects: result.projects, loader: false })
   ));
 }
 
 export const getOrderedProjectsAction = dispatch => ordered_by => {
-  request('/ordered', { ordered_by }).then(result => dispatch(
-    dispatchFormat(GET_ORDERED_PROJECTS, {projects: result.projects, loader: false})
+  request({ url: '/ordered', params: { ordered_by } }).then(result => dispatch(
+    dispatchFormat(GET_ORDERED_PROJECTS, { projects: result.projects, loader: false })
   ));
 }
 
-const request = async (url = '', params = '', method = 'GET') => {
+const request = async ({ url = '', params = '', method = 'GET' }) => {
   const options = {
     method,
     headers: { 'Content-Type': 'application/json', Authorization: `Bearer ${token}` }
--- src/views/projects/index.js
+++ src/views/projects/index.js
@@ -10,71 +10,68 @@ import HeaderProjects from '../../components/projects/header';
 import Project from '../../components/projects/project';
 
 const Projects = props => {
-	removeClassAndAddCurrentToThisView();
-	const { getAllProjectsMethod, getOrderedProjectsMethod, projectsArray, loader } = props;
+  removeClassAndAddCurrentToThisView();
+  const { getAllProjectsMethod, getOrderedProjectsMethod, projectsArray, loader } = props;
 
-	useEffect(() => {
+  useEffect(_ => {
     getAllProjectsMethod();
   }, []);
 
-  const getOrderedProjects = (event) => {
-    getOrderedProjectsMethod(event.target.value)
-  }
+  const getOrderedProjects = event => getOrderedProjectsMethod(event.target.value)
 
+  return (
+    <>
+      <HeaderProjects
+        title="Mis proyectos"
+        description="Crea y administra tus proyectos"
+      />
+      {loader ? (
+        <h1>Cargando</h1>
+      ) : (
+          <section className="projects">
 
-	return (
-		<>
-			<HeaderProjects
-				title="Mis proyectos"
-				description="Crea y administra tus proyectos"
-			/>
-			{loader ? (
-				<h1>Cargando</h1>
-			) : (
-				<section className="projects">
+            <select onChange={getOrderedProjects} style={{ width: '200px' }}>
+              <option value="created_at_asc">Creadas asc</option>
+              <option value="created_at_desc">Creadas desc</option>
+              <option value="updated_at_asc">Actualizado asc</option>
+              <option value="updated_at_desc">Actualizado desc</option>
+              <option value="name_asc">Nombre asc</option>
+              <option value="name_desc">Nombre desc</option>
+              <option value="delivery_date_asc">Fecha entrega asc</option>
+              <option value="delivery_date_desc">Fecha entrega desc</option>
+            </select>
 
-          <select onChange={getOrderedProjects} style={{width: '200px'}}>
-            <option value="created_at_asc">Creadas asc</option>
-            <option value="created_at_desc">Creadas desc</option>
-            <option value="updated_at_asc">Actualizado asc</option>
-            <option value="updated_at_desc">Actualizado desc</option>
-            <option value="name_asc">Nombre asc</option>
-            <option value="name_desc">Nombre desc</option>
-            <option value="delivery_date_asc">Fecha entrega asc</option>
-            <option value="delivery_date_desc">Fecha entrega desc</option>
-          </select>
-
-					<div className="projects__inner">
-						{projectsArray.map(project => (
-							<Project {...project} key={project.id} />
-						))}
-					</div>
-					{projectsArray.length >= 6 && (
-						<div className="projects__footer">
-							<button type="button" className="projects__footer__button-more">
-								Ver más
+            <div className="projects__inner">
+              {projectsArray.map(project => (
+                <Project {...project} key={project.id} />
+              ))}
+            </div>
+            {projectsArray.length >= 6 && (
+              <div className="projects__footer">
+                <button type="button" className="projects__footer__button-more">
+                  Ver más
 							</button>
-						</div>
-					)}
-				</section>
-			)}
-		</>
-	);
+              </div>
+            )}
+          </section>
+        )}
+    </>
+  );
 };
 
 Projects.propTypes = {
-	projectsArray: PropTypes.oneOfType([PropTypes.object, PropTypes.array]),
+  projectsArray: PropTypes.oneOfType([PropTypes.object, PropTypes.array]),
   getAllProjectsMethod: PropTypes.func.isRequired,
   getOrderedProjectsMethod: PropTypes.func.isRequired,
 };
 
 export default connect(
-	state => ({
-		projectsArray: state.projects.projects,
-		loader: state.projects.loader,
-	}),
-	dispath => ({
+  state => ({
+    projectsArray: state.projects.projects,
+    loader: state.projects.loader,
+  }),
+  dispath => ({
     getAllProjectsMethod: getAllProjectsAction(dispath),
     getOrderedProjectsMethod: getOrderedProjectsAction(dispath),
-	}),
+  }),
 )(Projects);
