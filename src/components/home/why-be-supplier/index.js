f204d29a5d2c8fbe9ed541d674c6495f6d10ebe4
Eduardo Alvarez <eduardo.a1993@gmail.com>
2020-01-13 16:23:56 -0300

    Finish desktop version

--- src/components/home/why-be-supplier/index.js
+++ src/components/home/why-be-supplier/index.js
@@ -3,15 +3,20 @@ import React from 'react';
 const WhyBeSupplier = () => (
 	<section className="supplier">
 		<div className="supplier__inner">
-			<div className="">
-				<h1 className="supplier__inner__title">¿Por qué ser proveedor?</h1>
-				<p className="supplier__inner__description">
+			<div className="supplier__inner__content">
+				<h1 className="supplier__inner__content__title">
+					¿Por qué ser proveedor?
+				</h1>
+				<p className="supplier__inner__content__description">
 					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non erat
 					tortor. In eu pulvinar ligula. Aenean leo purus, gravida id commodo
 					quis, elementum quis erat. Nam lobortis efficitur felis.
 				</p>
-				<div className="supplier__inner__bottom">
-					<button type="button" className="supplier__inner__bottom__btn">
+				<div className="supplier__inner__content__bottom">
+					<button
+						type="button"
+						className="supplier__inner__content__bottom__btn"
+					>
 						únete
 					</button>
 				</div>
