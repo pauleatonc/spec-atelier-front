fc500e2b34a5eea3c4795ee9f5ecb32600ff340b
Eduardo Alvarez <eduardo.a1993@gmail.com>
2020-01-15 22:50:02 -0300

    Modify menu and add lateral menu

--- src/components/navbar/navbar-top.js
+++ src/components/navbar/navbar-top.js
@@ -5,9 +5,15 @@ import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import LOGO from '@Assets/images/logo.png';
+import LOGO_MOBILE from '@Assets/images/logo_footer.png';
 import { getLocalStorage } from '@Helpers/localstorage.helper';
 import { presenterAction } from '@Actions';
 import Dropdown from '../dropdown';
+
+const handleShowLateralMenu = () => {
+  const menu = document.querySelector('.lateral_menu');
+  menu.classList.toggle('show');
+}
 
 const NavBar = props => {
 	const { presenterMethod } = props;
@@ -22,8 +28,67 @@ const NavBar = props => {
 							src={LOGO}
 							alt="Logotipo de SpecAtelier"
 						/>
+						<img
+							className="navbar__inner__logo-content__image__mobile"
+							src={LOGO_MOBILE}
+							alt="Logotipo de SpecAtelier"
+						/>
 					</Link>
 				</div>
+
+				<ul className="navbar__inner__section--mobile">
+					<button
+						className="navbar__inner__section--mobile__button"
+						onClick={handleShowLateralMenu}
+					>
+						<i className="fas fa-bars navbar__inner__section--mobile__button__icon" />
+					</button>
+
+					<nav className="lateral_menu">
+						<div className="lateral_menu__inner">
+							{!!getLocalStorage('token') && (
+								<li className="lateral_menu__inner__item">
+									<Link
+										to="/projects"
+										className="lateral_menu__inner__item__link"
+										data-view="projects"
+										onClick={() => presenterMethod('app')}
+									>
+										Proyectos
+									</Link>
+								</li>
+							)}
+							<li className="lateral_menu__inner__item">
+								<Link
+									to="/products"
+									className="lateral_menu__inner__item__link"
+									data-view="products"
+									onClick={() => presenterMethod('app')}
+								>
+									Productos
+								</Link>
+							</li>
+							<li className="lateral_menu__inner__item">
+								<Link
+									to="/brands"
+									className="lateral_menu__inner__item__link"
+									data-view="brands"
+									onClick={() => presenterMethod('app')}
+								>
+									Marcas
+								</Link>
+							</li>
+							<br />
+							<li className="lateral_menu__inner__item">
+								<Link to="/profile" className="lateral_menu__inner__item__link">
+									Perfil
+								</Link>
+							</li>
+
+							<li className="lateral_menu__inner__item">Cerrar sesión</li>
+						</div>
+					</nav>
+				</ul>
 
 				<ul className="navbar__inner__section">
 					<li className="navbar__inner__section__item">
