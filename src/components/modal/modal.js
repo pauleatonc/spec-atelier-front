8aa8427a6dd135f479347b0abdbeb0f0b735c13b
jonathan <jonathan.araya.m@gmail.com>
2020-04-04 04:54:07 -0300

    [modalProduct] moda to show product info

--- src/components/modal/modal.js
+++ src/components/modal/modal.js
@@ -1,8 +1,13 @@
 
 import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
+import { useDispatch } from 'react-redux';
+import { toggleModalProduct } from '@Actions/product.action';
 
-const Modal = ({ isOpen, size, toggle, children }) => {
+const Modal = ({ isOpen, size, children }) => {
+  const dispatch = useDispatch();
+  const onToogle = () => dispatch(toggleModalProduct());
+
   useEffect(() => {
     const { body } = document;
     if (isOpen) body.style['overflow-y'] = 'hidden';
@@ -10,8 +15,6 @@ const Modal = ({ isOpen, size, toggle, children }) => {
       body.style['overflow-y'] = 'scroll';
     };
   }, [isOpen]);
-  const onToogle = () => toggle(false);
-
   if (!isOpen) return null;
   return (
     <div tabIndex="-1" className="modal" role="dialog">
@@ -34,53 +37,12 @@ const Modal = ({ isOpen, size, toggle, children }) => {
 };
 
 Modal.propTypes = {
-  isOpen: PropTypes.bool.isRequired,
   size: PropTypes.oneOf(['sm', 'md', 'lg']),
-  toggle: PropTypes.func.isRequired,
   children: PropTypes.element,
-  product: PropTypes.shape({
-    id: PropTypes.number.isRequired,
-    name: PropTypes.string.isRequired,
-    short_description: PropTypes.string.isRequired,
-    long_description: PropTypes.string.isRequired,
-    system: PropTypes.shape({
-      name: PropTypes.string.isRequired,
-      id: PropTypes.number.isRequired,
-    }),
-    reference: PropTypes.string.isRequired,
-    brand: PropTypes.shape({
-      id: PropTypes.number.isRequired,
-      name: PropTypes.string.isRequired,
-    }),
-    dwg_url: PropTypes.string.isRequired,
-    bim_url: PropTypes.string.isRequired,
-    spec_pdf_url: PropTypes.arrayOf(PropTypes.string).isRequired,
-    images: PropTypes.arrayOf(
-      PropTypes.shape({
-        order: PropTypes.number,
-        url: PropTypes.string,
-      })
-    ),
-  })
-};
-
-const product = {
-  id: 1,
-  name: '',
-  short_description: '',
-  long_description: '',
-  system: {},
-  reference: '',
-  brand: {},
-  dwg_url: '',
-  bim_url: '',
-  spec_pdf_url: [],
-  images: [],
 };
 
 Modal.defaultProps = {
   size: 'md',
-  product,
   children: null,
 }
 
