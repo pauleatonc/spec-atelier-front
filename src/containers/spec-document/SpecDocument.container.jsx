80e113cdcbd7e8ee289ea738fd08af303371eb1c
Sebastian Rodriguez <ply.srh@gmail.com>
2020-04-19 02:18:13 -0400

    feat: add droopdown menu component feat: add menu to document container

--- src/containers/spec-document/SpecDocument.container.jsx
+++ src/containers/spec-document/SpecDocument.container.jsx
@@ -1,14 +1,30 @@
-import React from 'react';
-import { Root, Add } from './SpecDocument.styles';
+import React, { useState } from 'react';
+import DropdownMenu from '../../components/dropdown/DropdownMenu';
+import { Root, AddIcon, MenuItem } from './SpecDocument.styles';
 import specAddSource from '../../assets/images/icons/spec-add.svg';
 
 /**
  * The SpecDocument's container.
  */
 const SpecDocument = () => {
+  const [anchor, setAnchor] = useState(undefined);
+  const handleOpen = event => setAnchor(event.currentTarget);
+  const handleClose = () => setAnchor(undefined);
+
   return (
     <Root>
-      <Add alt="Agregar sección" src={specAddSource} />
+      <AddIcon alt="Agregar sección" src={specAddSource} onClick={handleOpen} />
+      <DropdownMenu
+        anchorRef={anchor}
+        offset={{ x: -15, y: -16 }}
+        open={Boolean(anchor)}
+        origin={{ x: 'right', y: 'top' }}
+        onClose={handleClose}
+      >
+        <MenuItem onClick={handleClose}>Añadir texto</MenuItem>
+        <MenuItem onClick={handleClose}>Añadir producto</MenuItem>
+        <MenuItem onClick={handleClose}>Crear producto</MenuItem>
+      </DropdownMenu>
     </Root>
   );
 };
