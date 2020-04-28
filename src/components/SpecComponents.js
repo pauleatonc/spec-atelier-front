82c78f37265656ae59c258bd3267dc908916226c
jonathan <jonathan.araya.m@gmail.com>
2020-04-28 19:08:48 -0400

    [specComponents] added  components

--- src/components/SpecComponents.js
+++ src/components/SpecComponents.js
@@ -1,7 +1,15 @@
 import Loading from './basics/loading';
 import Modal from './modal/modal';
+import NavbarLogin from './navbar/navbar-login/NavbarLogin.container';
+import NavbarApp from './navbar/navbar-app/NavbarApp.container';
+import TextInput from './inputs/TextInput';
+import ErrorMessage from './basics/ErrorMessage';
 
 export {
-  Modal,
+  ErrorMessage,
   Loading,
+  Modal,
+  NavbarLogin,
+  NavbarApp,
+  TextInput,
 }
