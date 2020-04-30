9a2a0f863c80b39cc8c16b1d1525a6e01a8e7679
jonathan <jonathan.araya.m@gmail.com>
2020-04-29 21:00:31 -0400

    [ProjectHeader] fix: styles on separated file

--- src/containers/projects-header/ProjectsHeader.jsx
+++ src/containers/projects-header/ProjectsHeader.jsx
@@ -1,50 +1,10 @@
 import React from 'react';
-import styled from 'styled-components';
 import {
-  COLOR_BLACK,
-} from '../../config/constants/styled-vars';
-
-const HEADER_VIEWS = '220px';
-
-const Container = styled.section`
-  background-image: url('${({ background }) => background}');
-  height: ${HEADER_VIEWS};
-  width: 100%;
-  color: ${COLOR_BLACK};
-  font-family: 'Akzidenz Grotesk - bold condenced';
-`;
-
-const Content = styled.div`
-  display: flex;
-  flex-direction: column;
-  margin-left: auto;
-  margin-right: auto;
-  justify-content: center;
-  height: 100%;
-  width: 90%;
-`;
-
-const Title = styled.h1`
-  font-size: 30px;
-  font-weight: bold;
-  font-stretch: normal;
-  font-style: normal;
-  line-height: normal;
-  letter-spacing: 1.88px;
-  text-transform: uppercase;
-`;
-
-const Description = styled.p`
-  font-size: 24px;
-  text-transform: uppercase;
-  font-family: Lato;
-  font-size: 16px;
-  font-weight: normal;
-  font-stretch: normal;
-  font-style: normal;
-  line-height: 1.58;
-  letter-spacing: 1px;
-`;
+  Container,
+  Content,
+  Title,
+  Description,
+} from './ProjectHeader.styles';
 
 const ProjectHeader = props => {
 	const { title, description, background } = props;
