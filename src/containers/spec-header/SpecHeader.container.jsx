46e1f6718b98d01097743f8fbad74e114c5cbe24
jonathan <jonathan.araya.m@gmail.com>
2020-05-01 18:39:16 -0400

    [specification] logo go to projects

--- src/containers/spec-header/SpecHeader.container.jsx
+++ src/containers/spec-header/SpecHeader.container.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { Root, Separator, Section, ProjectName, Download, Monetization } from './SpecHeader.styles';
 import logoSource from '../../assets/images/logo-spec.png';
 import logo2xSource from '../../assets/images/logo-spec@2x.png';
@@ -11,7 +12,9 @@ const SpecHeader = () => {
   return (
     <Root>
       <Section>
-        <img alt="Specatelier" src={logoSource} srcSet={`${logo2xSource} 2x, ${logo3xSource} 3x`} />
+        <Link to="/projects" data-view="projects">
+          <img alt="Specatelier" src={logoSource} srcSet={`${logo2xSource} 2x, ${logo3xSource} 3x`} />
+        </Link>
       </Section>
       <Separator />
       <ProjectName>Hospital San Carlos de ANCUD</ProjectName>
