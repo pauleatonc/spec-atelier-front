5bca426a48f104f289c36bde4d83c80c79a230cd
unknown <jonathan.araya.m@gmail.com>
2020-05-15 20:09:19 -0400

    [SpecComponents]

--- src/components/SpecComponents.js
+++ src/components/SpecComponents.js
@@ -2,11 +2,13 @@ import Loading from './basics/loading';
 import Modal from './modal/modal';
 import NavbarLogin from './navbar/navbar-login/NavbarLogin';
 import NavbarApp from './navbar/navbar-app/NavbarApp';
+import TextArea from './inputs/Textarea';
 import TextInput from './inputs/TextInput';
 import ErrorMessage from './basics/ErrorMessage';
 import Button from './buttons/Button';
 import Input from './inputs/Input';
 import StepBubbles from './basics/StepsBubbles';
+import Select from './inputs/Select';
 
 export {
   Button,
@@ -15,7 +17,9 @@ export {
   Modal,
   NavbarLogin,
   NavbarApp,
+  TextArea,
   TextInput,
   Input,
   StepBubbles,
+  Select,
 }
