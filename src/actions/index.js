0dc37d95e15cc7488f7751b4e819ef11cf08363f
unknown <jonathan.araya.m@gmail.com>
2020-05-27 03:00:23 -0400

    [actions] deleted project action

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,4 +2,3 @@ export * from './login.action';
 export * from './registration.action';
 export * from './recover_password.action';
 export * from './new_password.action';
-export * from './projects.action';
