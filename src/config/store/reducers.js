78309b5d774643fad18795130613b421c14a69ce
unknown <jonathan.araya.m@gmail.com>
2020-05-27 03:00:42 -0400

    [reducers] deletes unsed reducers

--- src/config/store/reducers.js
+++ src/config/store/reducers.js
@@ -2,7 +2,6 @@ import { combineReducers } from 'redux';
 import loginReducer from '../../reducers/login.reducer';
 import recoverPasswordReducer from '../../reducers/recover_password.reducer';
 import newPasswordReducer from '../../reducers/new_password.reducer';
-import projectsReducer from '../../reducers/projects.reducer';
 import alertReducer from '../../containers/alert/Alert.reducer';
 import specProductsSectionsReducer from '../../containers/spec-products-sections/SpecProductsSections.reducer';
 import specProductsItemsReducer from '../../containers/spec-products-items/SpecProductsItems.reducer';
@@ -11,7 +10,6 @@ import specModalPorductReducer from '../../containers/spec-modal-product/SpecMod
 import specModalReducer from '../../components/modal/modal.reducer';
 import specCreateProductReducer from '../../containers/spec-create-product/SpecCreateProduct.reducer';
 import authReducer from '../../containers/auth/auth.reducer';
-import projectsFiltersReducer from '../../containers/projects-filters/ProjectsFilters.reducer';
 import projectsListReducer from '../../containers/projects-list/ProjectsList.reducer';
 import newProjectReducer from '../../containers/project-create/ProjectCreate.reducer';
 import appReducer from '../../config/store/app-store/app.reducer';
@@ -23,8 +21,6 @@ export default combineReducers({
 	recoverPassword: recoverPasswordReducer,
   newPassword: newPasswordReducer,
   newProject: newProjectReducer,
-  projects: projectsReducer,
-  projectsFilters: projectsFiltersReducer,
   projectsList: projectsListReducer,
   alert: alertReducer,
   specProductsSections: specProductsSectionsReducer,
