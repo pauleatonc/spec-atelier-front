a54a90a02074768dddeadb87f7ef7c9ecd3a7819
unknown <jonathan.araya.m@gmail.com>
2020-05-28 02:56:31 -0400

    [services] requests helpers

--- src/config/store/helpers.js
+++ src/config/store/helpers.js
@@ -3,18 +3,5 @@
  */
 const onActionCreator = (type, payload) => ({ payload, type });
 
-/**
- *  delete null and undefined values
- *  transform { attrd: { id } } to { attr: id }
- *  return object 
- */
-
-export const cleanObject = obj => Object
-  .entries(obj)
-  .reduce((acc, [key, value]) => {
-    if (value === null || value === undefined) return acc;
-    if (value && typeof value === 'object' && value.id) return { ...acc, [key]: value.id }
-    return { ...acc, [key]: value }
-  }, {});
 
 export default onActionCreator;
