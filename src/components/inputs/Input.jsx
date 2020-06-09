7b2ea3025ddda58a082c3da52114c2cbd72399f8
unknown <jonathan.araya.m@gmail.com>
2020-06-09 03:49:08 -0400

    [Input] Underline input

--- src/components/inputs/Input.jsx
+++ src/components/inputs/Input.jsx
@@ -1,17 +1,22 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Root, Label, Input as InputBase } from './Input.styles';
+import { Root, Label, Input as InputBase, InputUnderline } from './Input.styles';
 
 /**
  * The Input's component.
  */
 const Input = props => {
-  const { label, placeholder, value, width, onChange, name } = props;
+  const { label, placeholder, value, width, onChange, name, variant, type, disabled } = props;
 
   return (
     <Root width={width}>
       {label && <Label>{label}</Label>}
-      <InputBase name={name} placeholder={placeholder} type="text" value={value} onChange={onChange} />
+      {type === 'default'
+        && <InputBase name={name} placeholder={placeholder} type="text" value={value} onChange={onChange} />
+      }
+      {type === 'underline' &&
+        <InputUnderline name={name} disabled={disabled} placeholder={placeholder} variant={variant} type="text" value={value}  onChange={onChange} />
+      }
     </Root>
   );
 };
@@ -21,6 +26,8 @@ Input.defaultProps = {
   placeholder: '',
   width: '100%',
   name: '',
+  type: 'default',
+  disabled: false,
 };
 Input.propTypes = {
   label: PropTypes.string,
@@ -29,6 +36,8 @@ Input.propTypes = {
   value: PropTypes.string.isRequired,
   width: PropTypes.string,
   onChange: PropTypes.func.isRequired,
+  type: PropTypes.oneOf(['default', 'underline']),
+  disabled: PropTypes.bool,
 };
 
 export default Input;
--- src/components/inputs/Input.styles.jsx
+++ src/components/inputs/Input.styles.jsx
@@ -1,5 +1,6 @@
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
+import { COLOR_LIGHTGREY, COLOR_WHITE, COLOR_BLACK } from '../../config/constants/styled-vars';
 
 export const Root = styled.div`
   width: ${({ width = '100%' }) => width};
@@ -38,4 +39,25 @@ export const Input = styled.input`
   &::placeholder {
     color: rgba(33, 33, 33, 0.46);
   }
+`;
+
+export const InputUnderline = styled.input`
+  border: 0; 
+  outline: 0;
+  min-width: 100px;
+  width: 100%;
+  display: flex;
+  padding: 10px 10px 10px 0;
+  flex: 1;
+  border-bottom: 1px solid ${COLOR_LIGHTGREY};
+  font-family: Lato;
+  font-size: 14px;
+  font-weight: normal;
+  font-stretch: normal;
+  font-style: normal;
+  line-height: normal;
+  letter-spacing: 1.17px;
+  color: ${COLOR_BLACK};
+  background-color: ${COLOR_WHITE};
+  border-radius: 0;
 `;
