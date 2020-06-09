0e5010b19bf7456b9e9e86b2a51d897725d26ed9
unknown <jonathan.araya.m@gmail.com>
2020-06-09 03:52:27 -0400

    [SpecComponents] added new components

--- src/components/SpecComponents.js
+++ src/components/SpecComponents.js
@@ -1,3 +1,4 @@
+import Image from './image/Image';
 import Loading from './basics/loading';
 import Modal from './modal/modal';
 import NavbarLogin from './navbar/navbar-login/NavbarLogin';
@@ -6,6 +7,7 @@ import TextArea from './inputs/Textarea';
 import TextInput from './inputs/TextInput';
 import ErrorMessage from './basics/ErrorMessage';
 import Button from './buttons/Button';
+import ButtonLink from './buttons/ButtonLink';
 import Input from './inputs/Input';
 import StepBubbles from './basics/StepsBubbles';
 import Select from './inputs/Select';
@@ -14,7 +16,9 @@ import SearchBar from './filters/SearchBar';
 
 export {
   Button,
+  ButtonLink,
   ErrorMessage,
+  Image,
   Loading,
   Modal,
   NavbarLogin,
