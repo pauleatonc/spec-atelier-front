93bea5d9615b91314d70fc54166679900d1e61ad
unknown <jonathan.araya.m@gmail.com>
2020-06-09 03:42:57 -0400

    [ButtonGoogle] WIP

---
+++ src/components/buttons/ButtonGoogle.styles.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+import { COLOR_WHITE } from '../../config/constants/styled-vars';
+
+export const Text = styled.span`
+  font-family: Lato;
+  font-size: 16px;
+  font-weight: bold;
+  font-stretch: normal;
+  font-style: normal;
+  line-height: normal;
+  letter-spacing: normal;
+  color: ${COLOR_WHITE};
+  margin: 2px 2px 4px 8px;
+`;
