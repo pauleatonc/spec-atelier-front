f0a1ff50e085f2b76c5dcfa934f1dbba15f555c1
Sebastian Rodriguez <sebastian@n3twork.com>
2020-06-29 22:13:00 -0400

    chore: fix rebase breaking changes

--- src/modules/services.js
+++ src/modules/services.js
@@ -26,6 +26,19 @@ export const cleanObject = obj => Object
     return { ...acc, [key]: value }
   }, {});
 
+/**
+ *  Delete null, undefined and empty strings values from object
+ *  transform { attrd: { id } } to { attr: id }
+ *  return a new object
+ */
+export const cleanParams = obj => Object
+  .entries(obj)
+  .reduce((acc, [key, value]) => {
+    if (value === null || value === undefined || value === '') return acc;
+    if (value && typeof value === 'object' && value.id) return { ...acc, [key]: value.id };
+    return { ...acc, [key]: value }
+  }, {});
+
 /** 
  * Format Object of params to string
  */
