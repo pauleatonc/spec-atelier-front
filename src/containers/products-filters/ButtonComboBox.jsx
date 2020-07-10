b25fd764d9976e0f3467afa0f0b4f909752f96f3
Ibox <jonathan.araya.m@gmail.com>
2020-07-09 22:32:27 -0400

    [styles] ButtonComboBox

--- src/containers/products-filters/ButtonComboBox.jsx
+++ src/containers/products-filters/ButtonComboBox.jsx
@@ -1,20 +1,49 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useComboBox } from '../../components/inputs/Inputs.hooks';
-import ToggleMenu from '../../components/menus/ToggleMenu';
 import ComboBox from '../../components/inputs/ComboBox';
 import { Button } from '../../components/SpecComponents';
 
 import {
   getProductsByFilter,
 } from '../products-list/ProductsList.actions';
+import { Container, Content } from './ButtonComboBox.styles';
+
+const useOuterClick = callback => {
+  const innerRef = useRef();
+  const callbackRef = useRef();
+
+  useEffect(() => {
+    callbackRef.current = callback;
+  });
+
+  useEffect(() => {
+
+    const handleClick = e => {
+      if (
+        innerRef.current &&
+        callbackRef.current &&
+        !innerRef.current.contains(e.target)
+      ) {
+        callbackRef.current(e);
+      }
+    }
+    document.addEventListener("click", handleClick);
+    return () => document.removeEventListener("click", handleClick);
+  }, []);
+  return innerRef;
+}
 
 const mapToSelector = ({ name = '', id }) => ({ id, label: name || '', value: id || name });
 
-const ButtonComboBox = ({ options, name, children, absolute }) => {
+const ButtonComboBox = ({ options = [], name, children }) => {
   const { isSelectedAll, filters } = useSelector(state => state.productsList);
   const dispatch = useDispatch();
   const submitCallback = val => dispatch(getProductsByFilter({ ...filters, [name]: val }));
+  const [isOpen, setIsOpen] = useState(false);
+  const toggle = () => setIsOpen(!isOpen);
+
+  const innerRef = useOuterClick(() => setIsOpen(false));
 
   const { values, set, onSubmit } = useComboBox({ initialValue: [], submitCallback });
 
@@ -25,30 +54,26 @@ const ButtonComboBox = ({ options, name, children, absolute }) => {
   }, [isSelectedAll]);
 
   return (
-    <ToggleMenu
-      absolute={absolute}
-      anchor={(
-        <Button
-          inverse
-          selected={values.length}
-          variant={values.length ? 'secondary' : 'default'}
-          onClick={() => { }}
-        >
-          {children}
-        </Button>
-      )}
-    >{() => (
-      <ComboBox
-        options={options.map(mapToSelector)}
-        placeholder="Selecciona"
-        type="underline"
-        values={values}
-        onChange={onSubmit}
-        optionAll
-        absolute
-      />
-    )}
-    </ToggleMenu>
+    <Container isOpen={isOpen} ref={innerRef}>
+      <Button
+        inverse
+        selected={values.length}
+        variant={values.length ? 'secondary' : 'default'}
+        onClick={toggle}
+      >
+        {children}
+      </Button>
+      <Content isOpen={isOpen}>
+        <ComboBox
+          options={options.map(mapToSelector)}
+          placeholder="Selecciona"
+          type="underline"
+          values={values}
+          onChange={onSubmit}
+          optionAll
+        />
+      </Content>
+    </Container>
   );
 }
 
