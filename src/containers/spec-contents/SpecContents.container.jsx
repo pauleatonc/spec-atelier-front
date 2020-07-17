167d9b40e0b67793fc9cb874cd76fd74a1e74efe
Sebastian Rodriguez <sebastian@n3twork.com>
2020-07-17 04:41:36 -0400

    feat: add collapsible component feat: add arrow icons feat: show list of blocks

--- src/containers/spec-contents/SpecContents.container.jsx
+++ src/containers/spec-contents/SpecContents.container.jsx
@@ -1,18 +1,112 @@
-import React from 'react';
+import React, { Fragment, useEffect, useMemo, useState } from 'react';
 import { useSelector } from 'react-redux';
-import { Root } from './SpecContents.styles';
+import { useHistory } from 'react-router-dom';
+import Collapsible from '../../components/basics/Collapsible';
+import { Root, PanelTitle, ListTitle, ListItem, ArrowIcon } from './SpecContents.styles';
+import arrowDownSource from '../../assets/images/icons/arrow-down.svg';
+import arrowUpSource from '../../assets/images/icons/arrow-up.svg';
 
 /**
  * The SpecContents' container.
  */
 const SpecContents = () => {
   const { show } = useSelector(state => state.specContents);
+  const { blocks } = useSelector(state => state.specDocument);
+  const history = useHistory();
+  const sections = useMemo(() => {
+    const sectionsBlocks = blocks.filter(block => block.type === 'section');
+    
+    return sectionsBlocks.map(sectionBlock => ({
+      ...sectionBlock,
+      items: blocks
+        .filter(block => block.type === 'item' && block.sectionID === sectionBlock.id)
+        .map(itemBlock => ({
+          ...itemBlock,
+          products: blocks.filter(block => block.type === 'product' && block.itemID === itemBlock.id),
+        })),
+    }));
+  }, [blocks]);
+  const [selectedSection, setSelectedSection] = useState();
+  const [selectedItem, setSelectedItem] = useState();
+  const handleSectionClick = sectionID => () => {
+    setSelectedItem();
+    setSelectedSection(currentSelectedSection => {
+      if (currentSelectedSection === sectionID) {
+        return undefined;
+      }
+
+      history.push(`#${sectionID}`);
+
+      return sectionID;
+    });
+  };
+  const handleItemClick = itemID => () => {
+    setSelectedItem(currentSelectedItem => {
+      if (currentSelectedItem === itemID) {
+        return undefined;
+      }
+
+      history.push(`#${itemID}`);
+
+      return itemID;
+    });
+  };
+  const handleProductClick = productID => () => {
+    history.push(`#${productID}`);
+  };
+
+  useEffect(() => {
+    if (show) {
+      return;
+    }
+
+    setSelectedSection();
+    setSelectedItem();
+  }, [show]);
 
   return (
     <Root show={show}>
-      SPEC CONTENTS
+      <PanelTitle>Tabla de contenidos</PanelTitle>
+      <ListTitle>Indice de Partidas</ListTitle>
+      {sections.map((section, sectionIndex) => (
+        <Fragment key={section.id}>
+          <ListItem title={section.element.title} onClick={handleSectionClick(section.id)}>
+            <span>
+              {`${sectionIndex + 1}. ${section.element.title}`}
+            </span>
+            <ArrowIcon src={selectedSection === section.id ? arrowUpSource : arrowDownSource} />
+          </ListItem>
+          <Collapsible show={selectedSection === section.id}>
+            {section.items.map((item, itemIndex) => (
+              <Fragment key={item.id}>
+                <ListItem padding="0 23px 0 62px" title={item.element.title} onClick={handleItemClick(item.id)}>
+                  <span>
+                    {`${sectionIndex + 1}.${itemIndex + 1}. ${item.element.title}`}
+                  </span>
+                  <ArrowIcon src={selectedItem === item.id ? arrowUpSource : arrowDownSource} />
+                </ListItem>
+                <Collapsible show={selectedItem === item.id}>
+                  {item.products.map((product, productIndex) => (
+                    <ListItem
+                      key={product.id}
+                      padding="0 23px 0 77px"
+                      title={product.element.title}
+                      onClick={handleProductClick(product.id)}
+                    >
+                      <span>
+                        {`${sectionIndex + 1}.${itemIndex + 1}.${productIndex + 1}. ${product.element.title}`}
+                      </span>
+                      <span>&nbsp;</span>
+                    </ListItem>
+                  ))}
+                </Collapsible>
+              </Fragment>
+            ))}
+          </Collapsible>
+        </Fragment>
+      ))}
     </Root>
-  )
+  );
 };
 
 export default SpecContents;
--- src/containers/spec-contents/SpecContents.styles.jsx
+++ src/containers/spec-contents/SpecContents.styles.jsx
@@ -1,6 +1,6 @@
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
-import { COLOR_WHITE } from '../../config/constants/styled-vars';
+import { COLOR_WHITE, COLOR_MINE_SHAFT } from '../../config/constants/styled-vars';
 
 export const Root = styled.div`
   background-color: ${COLOR_WHITE};
@@ -19,3 +19,63 @@ Root.defaultProps = {
 Root.propTypes = {
   show: PropTypes.bool,
 };
+
+export const PanelTitle = styled.section`
+  align-items: center;
+  display: flex;
+  color: ${COLOR_MINE_SHAFT};
+  font-family: Lato;
+  font-size: 13px;
+  font-weight: bold;
+  height: 46px;
+  letter-spacing: 1.08px;
+  padding: 0 23px;
+  width: 100%;
+`;
+
+export const ListTitle = styled.section`
+  align-items: center;
+  color: ${COLOR_MINE_SHAFT};
+  display: flex;
+  font-family: Lato;
+  font-size: 13px;
+  height: 30px;
+  letter-spacing: 1.08px;
+  margin: 10px 0 0;
+  padding: 0 23px;
+`;
+
+export const ListItem = styled.section`
+  align-items: center;
+  color: ${COLOR_MINE_SHAFT};
+  cursor: pointer;
+  display: grid;
+  font-family: Lato;
+  font-size: 13px;
+  grid-template-columns: auto 24px;
+  height: 30px;
+  letter-spacing: 1.08px;
+  padding: ${({ padding = '0 23px 0 43px' }) => padding};
+
+  &:hover {
+    background-color: rgba(235, 234, 234, 0.5);
+  }
+
+  & span {
+    overflow-x: hidden;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+  }
+`;
+
+ListItem.defaultProps = {
+  padding: '0 23px 0 43px',
+};
+ListItem.propTypes = {
+  padding: PropTypes.string,
+};
+
+export const ArrowIcon = styled.img`
+  pointer-event: none;
+  user-select: none;
+`;
