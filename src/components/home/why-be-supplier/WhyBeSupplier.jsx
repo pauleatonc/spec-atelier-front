f2f1bcfb4c024270f8d5d84aa77f90c1fe6422ff
Ibox <jonathan.araya.m@gmail.com>
2020-11-12 18:49:56 -0300

    [Landing]  button "unete" to login

--- src/components/home/why-be-supplier/WhyBeSupplier.jsx
+++ src/components/home/why-be-supplier/WhyBeSupplier.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useHistory } from 'react-router';
 import {
 	Container,
 	Content,
@@ -9,24 +10,26 @@ import {
 } from './WhyBeSupplier.styles';
 import Button from '../../buttons/Button';
 
-const WhyBeSupplier = () => (
-	<Container>
-		<Content>
-			<InfoContainer>
-				<Title>
-					¿Por qué ser proveedor?
+const WhyBeSupplier = () => {
+	const history = useHistory();
+	return (
+		<Container>
+			<Content>
+				<InfoContainer>
+					<Title>
+						¿Por qué ser proveedor?
 				</Title>
-				<Description>
-					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non erat
-					tortor. In eu pulvinar ligula. Aenean leo purus, gravida id commodo
-					quis, elementum quis erat. Nam lobortis efficitur felis.
+					<Description>
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non erat
+						tortor. In eu pulvinar ligula. Aenean leo purus, gravida id commodo
+						quis, elementum quis erat. Nam lobortis efficitur felis.
 				</Description>
-				<ButtonContainer>
-					<Button variant="primary" width="100%" onClick={() => {}}>únete</Button>
-				</ButtonContainer>
-			</InfoContainer>
-		</Content>
-	</Container>
-);
-
+					<ButtonContainer>
+						<Button variant="primary" width="100%" onClick={() => history.push('/login')}>únete</Button>
+					</ButtonContainer>
+				</InfoContainer>
+			</Content>
+		</Container>
+	);
+};
 export default WhyBeSupplier;
