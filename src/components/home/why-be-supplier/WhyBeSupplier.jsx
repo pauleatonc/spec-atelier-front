994c932f152bc95ecf63af5b27df19315d609c0f
unknown <jonathan.araya.m@gmail.com>
2020-11-24 21:42:45 -0300

    [Home] boton "unete" to login

--- src/components/home/why-be-supplier/WhyBeSupplier.jsx
+++ src/components/home/why-be-supplier/WhyBeSupplier.jsx
@@ -8,25 +8,29 @@ import {
 	ButtonContainer,
 } from './WhyBeSupplier.styles';
 import Button from '../../buttons/Button';
+import { useHistory } from 'react-router';
 
-const WhyBeSupplier = () => (
-	<Container>
-		<Content>
-			<InfoContainer>
-				<Title>
-					¿Quieres ser proveedor?
-				</Title>
-				<Description>
-					Publica tus productos y soluciones costructivas en SpecAtelier
-					para que nuestros usuario puedan especificarlas en sus proyectos.
-					Aumenta la demanda con un solo click
-				</Description>
-				<ButtonContainer>
-					<Button variant="primary" width="100%" onClick={() => {}}>únete</Button>
-				</ButtonContainer>
-			</InfoContainer>
-		</Content>
-	</Container>
-);
+const WhyBeSupplier = () => {
+  const history = useHistory();
+  return (
+    <Container>
+      <Content>
+        <InfoContainer>
+          <Title>
+            ¿Quieres ser proveedor?
+          </Title>
+          <Description>
+            Publica tus productos y soluciones costructivas en SpecAtelier
+            para que nuestros usuario puedan especificarlas en sus proyectos.
+            Aumenta la demanda con un solo click
+          </Description>
+          <ButtonContainer>
+            <Button variant="primary" width="100%" onClick={() => history.push('/login')}>únete</Button>
+          </ButtonContainer>
+        </InfoContainer>
+      </Content>
+    </Container>
+  );
+};
 
 export default WhyBeSupplier;
