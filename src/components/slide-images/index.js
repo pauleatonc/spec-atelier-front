39b3e5fcf1e3adcc541f56b2b07e708d29ad7247
Jonathan <jonathan.araya.m@gmail.com>
2021-01-09 16:15:19 -0300

    [slides]

--- src/components/slide-images/index.js
+++ src/components/slide-images/index.js
@@ -5,88 +5,57 @@ import ANSELMI from '@Assets/images/home/slide/anselmi.png';
 import HOPPE from '@Assets/images/home/slide/hoppe.png';
 import SIMONSWERK from '@Assets/images/home/slide/simonswerk.png';
 import SOLECO from '@Assets/images/home/slide/soleco.png';
+import Image from '../image/Image';
 
-const data = [
-	{
-		id: 1,
-		altImage: 'Andreu',
-		pathImage: ANDREU,
-	},
-	{
-		id: 2,
-		altImage: 'Anselmi',
-		pathImage: ANSELMI,
-	},
-	{
-		id: 3,
-		altImage: 'Hoppe',
-		pathImage: HOPPE,
-	},
-	{
-		id: 4,
-		altImage: 'Simonswerk',
-		pathImage: SIMONSWERK,
-	},
-	{
-		id: 5,
-		altImage: 'Soleco',
-		pathImage: SOLECO,
-	},
-	{
-		id: 6,
-		altImage: 'Tesla',
-		pathImage: ANDREU,
-	},
-];
-
-const NextArrow = props => {
-	const { onClick } = props;
-	return (
-		<div className="container__row next" onClick={onClick}>
-			<i className="fas fa-angle-right" />
-		</div>
-	);
+const NextArrow = (props) => {
+  const { onClick } = props;
+  return (
+    <div className="container__row next" onClick={onClick}>
+      <i className="fas fa-angle-right" />
+    </div>
+  );
 };
 
-const PrevArrow = props => {
-	const { onClick } = props;
-	return (
-		<div className="container__row prev" onClick={onClick}>
-			<i className="fas fa-angle-left" />
-		</div>
-	);
+const PrevArrow = (props) => {
+  const { onClick } = props;
+  return (
+    <div className="container__row prev" onClick={onClick}>
+      <i className="fas fa-angle-left" />
+    </div>
+  );
 };
 
 const settings = {
-	className: 'container',
-	dots: false,
-	infinite: true,
-	slidesToShow: 5,
-	slidesToScroll: 1,
-	swipeToSlide: true,
-	arrows: true,
-	nextArrow: <NextArrow />,
-	prevArrow: <PrevArrow />,
-	responsive: [
-		{
-			breakpoint: 1024,
-			settings: {
-				slidesToShow: 5,
-			},
-		},
-		{
-			breakpoint: 768,
-			settings: {
-				slidesToShow: 2,
-			},
-		},
-		{
-			breakpoint: 375,
-			settings: {
-				slidesToShow: 1,
-			},
-		},
-	],
+  className: 'container',
+  dots: false,
+  infinite: true,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  swipeToSlide: true,
+  arrows: true,
+  lazyLoad: true,
+  nextArrow: <NextArrow />,
+  prevArrow: <PrevArrow />,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+      },
+    },
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 2,
+      },
+    },
+    {
+      breakpoint: 375,
+      settings: {
+        slidesToShow: 1,
+      },
+    },
+  ],
 };
 
 const handlePrintImages = obj =>
@@ -94,21 +63,21 @@ const handlePrintImages = obj =>
 		<div key={data.id} className="container__images_content">
 			<img
 				className="container__images_content__image"
-				src={data.pathImage}
+				src={data.logo}
 				alt={data.altImage}
-			/>
-		</div>
-	));
+      />
+    </div>
+  ));
 
-const SliderImages = () => {
-	return (
-		<div className="slide">
-			<div className="slide__inner">
-				<h1 className="slide__inner__title">Principales colaboradores</h1>
-				<Slider {...settings}>{handlePrintImages(data)}</Slider>
-			</div>
-		</div>
-	);
+const SliderImages = ({ images }) => {
+  return (
+    <div className="slide">
+      <div className="slide__inner">
+        <h1 className="slide__inner__title">Principales colaboradores</h1>
+        <Slider {...settings}>{handlePrintImages(images)}</Slider>
+      </div>
+    </div>
+  );
 };
 
 export default SliderImages;
