6964125fd366d67a03a626962aa5bc1a7cc52b8f
Andy Olarte <andyolarte@MacBook-Pro-de-Wolox.local>
2021-02-13 13:51:02 -0500

    change brands for clients in collaborator list

--- src/services/brands.service.js
+++ src/services/brands.service.js
@@ -5,19 +5,29 @@ import { cleanObject, factoryService, formatParams } from '../modules/services';
 /**
  * Gets the list of collaborators by params (page, limit, sort, keywords).
  */
-export const getBrands = factoryService(
-  (params) => getJsonRequest(`${API_BASE_URL}/brands${formatParams(params || {})}`),
+export const getBrands = factoryService((params) =>
+	getJsonRequest(`${API_BASE_URL}/brands${formatParams(params || {})}`),
 );
 
 /**
  * Create new Brand.
  */
-export const createNewBrand = factoryService(
-  ({ userId, brand }) => postJsonRequest(`${API_BASE_URL}/users/${userId}/brands/`, { brand: cleanObject(brand) }),
+export const createNewBrand = factoryService(({ userId, brand }) =>
+	postJsonRequest(`${API_BASE_URL}/users/${userId}/brands/`, {
+		brand: cleanObject(brand),
+	}),
 );
 
 /**
  * Gets the list of collaborators by params (page, limit, sort, keywords).
  */
-export const getBrand = factoryService(brandId => getJsonRequest(`${API_BASE_URL}/brands/${brandId}`));
+export const getBrand = factoryService((brandId) =>
+	getJsonRequest(`${API_BASE_URL}/brands/${brandId}`),
+);
 
+/**
+ * Gets the list of collaborators by params (page, limit, sort, keywords).
+ */
+export const getClients = factoryService((params) =>
+	getJsonRequest(`${API_BASE_URL}/clients${formatParams(params || {})}`),
+);
