de729cbb4c8a75fdd6db1c7567abc7417478c586
Simba <nosoyleon@gmail.com>
2021-04-16 23:30:14 -0500

    Update: section and item filter in spec

--- src/containers/spec-products-items/SpecProductsItems.container.jsx
+++ src/containers/spec-products-items/SpecProductsItems.container.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { onGetSpecProductsByItem, onGetSpecProductsBySection } from '../spec-products/SpecProducts.actions';
+import {
+	onGetSpecProductsByItem,
+	onGetSpecProductsBySection,
+} from '../spec-products/SpecProducts.actions';
+import { setFilters } from '../products-list/ProductsList.actions';
 import { onHideSpecProductsItemsSuccess } from './SpecProductsItems.actions';
 import Breadcrumbs from '../../components/basics/Breadcrumbs';
 import { Root, Header, Body, Loading, Item } from './SpecProductsItems.styles';
@@ -9,39 +13,55 @@ import { Root, Header, Body, Loading, Item } from './SpecProductsItems.styles';
  * The SpecProductsItems' container.
  */
 const SpecProductsItems = () => {
-  const { item: selectedItemID } = useSelector(state => state.specProducts.filters);
-  const { collection: items, show } = useSelector(state => state.specProductsItems);
-  const dispatch = useDispatch();
-  const handleShowSections = () => {
-    dispatch(onGetSpecProductsBySection({ sectionID: '', itemID: '' }));
-    dispatch(onHideSpecProductsItemsSuccess());
-  }
-  const handleItemClick = itemID => () => itemID ? dispatch(onGetSpecProductsByItem({ itemID })) : () => {};
+	const { item: selectedItemID } = useSelector(
+		(state) => state.specProducts.filters,
+	);
+	const { collection: items, show } = useSelector(
+		(state) => state.specProductsItems,
+	);
+	const dispatch = useDispatch();
+	const handleShowSections = () => {
+		dispatch(setFilters({ section: [], item: [] }));
+		dispatch(onGetSpecProductsBySection({ sectionID: '', itemID: '' }));
+		dispatch(onHideSpecProductsItemsSuccess());
+	};
+	const handleItemClick = (itemID) => () => {
+		if (itemID) {
+			dispatch(setFilters({ item: [itemID] }));
+			dispatch(onGetSpecProductsByItem({ itemID }));
+		}
+	};
 
-  if (!show) {
-    return null;
-  }
+	if (!show) {
+		return null;
+	}
 
-  return (
-    <Root>
-      <Header>
-        <Breadcrumbs items={[{ label: 'Secciones', onClick: handleShowSections }, { label: 'Partidas' }]} />
-      </Header>
-      {items.length === 0 && <Loading>Cargando...</Loading>}
-      {items.length > 0 && (
-        <Body>
-          {items.map(item =>
-            <Item
-              active={item.id === selectedItemID}
-              key={item.id}
-              onClick={handleItemClick(item.id)}>
-              {item.code}. {item.name}
-            </Item>
-          )}
-        </Body>
-      )}
-    </Root>
-  )
+	return (
+		<Root>
+			<Header>
+				<Breadcrumbs
+					items={[
+						{ label: 'Secciones', onClick: handleShowSections },
+						{ label: 'Partidas' },
+					]}
+				/>
+			</Header>
+			{items.length === 0 && <Loading>Cargando...</Loading>}
+			{items.length > 0 && (
+				<Body>
+					{items.map((item) => (
+						<Item
+							active={item.id === selectedItemID}
+							key={item.id}
+							onClick={handleItemClick(item.id)}
+						>
+							{item.code}. {item.name}
+						</Item>
+					))}
+				</Body>
+			)}
+		</Root>
+	);
 };
 
 export default SpecProductsItems;
--- src/containers/spec-products/SpecProducts.reducer.js
+++ src/containers/spec-products/SpecProducts.reducer.js
@@ -1,23 +1,8 @@
 import {
-	GET_SPEC_PRODUCTS,
-	GET_SPEC_PRODUCTS_ERROR,
-	GET_SPEC_PRODUCTS_SUCCESS,
-	GET_SPEC_PRODUCTS_BY_PAGE,
-	GET_SPEC_PRODUCTS_BY_PAGE_ERROR,
-	GET_SPEC_PRODUCTS_BY_PAGE_SUCCESS,
 	HIDE_SPEC_PRODUCTS_SUCCESS,
 	SHOW_SPEC_PRODUCTS_SUCCESS,
-	UPDATE_SPEC_PRODUCTS_FILTERS,
-	UPDATE_SPEC_PRODUCTS_FILTERS_ALL,
 	UPDATE_SPEC_PRODUCTS_FILTER_ITEM,
-	UPDATE_SPEC_PRODUCTS_FILTER_KEYWORD,
 	UPDATE_SPEC_PRODUCTS_FILTER_SECTION,
-	UPDATE_SPEC_PRODUCTS_FILTER_SORT,
-	UPDATE_SPEC_PROJECT_TYPE_FILTERS,
-	GET_SPEC_MYSPEC,
-	GET_SPEC_MYSPEC_ERROR,
-	GET_SPEC_ROOMTYPES,
-	GET_SPEC_ROOMTYPES_ERROR,
 } from './SpecProducts.actions';
 
 const specProductsState = {
@@ -43,43 +28,6 @@ const specProductsState = {
  */
 const specProductsReducer = (state = specProductsState, { payload, type }) => {
 	switch (type) {
-		case GET_SPEC_PRODUCTS: {
-			return {
-				...state,
-				collection: [],
-				loading: true,
-				nextPage: null,
-				total: 0,
-			};
-		}
-		case GET_SPEC_PRODUCTS_BY_PAGE: {
-			return { ...state, loading: true };
-		}
-		case GET_SPEC_PRODUCTS_ERROR:
-		case GET_SPEC_PRODUCTS_BY_PAGE_ERROR: {
-			return { ...state, loading: false };
-		}
-		case GET_SPEC_PRODUCTS_SUCCESS:
-		case GET_SPEC_PRODUCTS_BY_PAGE_SUCCESS: {
-			return {
-				...state,
-				collection: payload.products || [],
-				loading: false,
-				nextPage: payload.nextPage || null,
-				total: payload.total || 0,
-			};
-		}
-		case UPDATE_SPEC_PROJECT_TYPE_FILTERS: {
-			return {
-				...state,
-				filters: {
-					...state.filters,
-					project_types:
-						payload.project_types || state.filters.project_types || [],
-					room_types: payload.room_types || state.filters.room_types || [],
-				},
-			};
-		}
 		// eslint-disable-next-line no-lone-blocks
 		case HIDE_SPEC_PRODUCTS_SUCCESS: {
 			return { ...specProductsState, show: false };
@@ -87,45 +35,13 @@ const specProductsReducer = (state = specProductsState, { payload, type }) => {
 		case SHOW_SPEC_PRODUCTS_SUCCESS: {
 			return { ...state, show: true };
 		}
-		case UPDATE_SPEC_PRODUCTS_FILTERS: {
-			return {
-				...state,
-				filters: {
-					...state.filters,
-					[payload.key]:
-						typeof payload.value === 'object'
-							? payload.value.map((val) => val.value)
-							: payload.value,
-				},
-			};
-		}
-		case UPDATE_SPEC_PRODUCTS_FILTERS_ALL: {
-			return {
-				...state,
-				filters: {
-					...specProductsState.filters,
-					item: state.filters.item,
-					keyword: state.filters.keyword,
-					section: state.filters.section,
-					sort: state.filters.sort,
-				},
-			};
-		}
+
 		case UPDATE_SPEC_PRODUCTS_FILTER_ITEM: {
 			return {
 				...state,
 				filters: {
 					...state.filters,
 					item: payload.itemID,
-				},
-			};
-		}
-		case UPDATE_SPEC_PRODUCTS_FILTER_KEYWORD: {
-			return {
-				...state,
-				filters: {
-					...state.filters,
-					keyword: payload.keyword,
 				},
 			};
 		}
@@ -137,39 +53,6 @@ const specProductsReducer = (state = specProductsState, { payload, type }) => {
 					section: payload.sectionID,
 					item: payload.itemID,
 				},
-			};
-		}
-		case UPDATE_SPEC_PRODUCTS_FILTER_SORT: {
-			return {
-				...state,
-				filters: {
-					...state.filters,
-					sort: payload.sort,
-				},
-			};
-		}
-		case GET_SPEC_MYSPEC: {
-			return {
-				...state,
-				specifications: payload.specifications || [],
-			};
-		}
-		case GET_SPEC_MYSPEC_ERROR: {
-			return {
-				...state,
-				specifications: [],
-			};
-		}
-		case GET_SPEC_ROOMTYPES: {
-			return {
-				...state,
-				room_types: payload.room_types || [],
-			};
-		}
-		case GET_SPEC_ROOMTYPES_ERROR: {
-			return {
-				...state,
-				room_types: [],
 			};
 		}
 		default: {
