d57658e7b5a3942eba63ccbc4cd1e7edf21c8f74
Andy Olarte <andyolarte@MacBook-Pro-de-Wolox.local>
2021-05-10 18:39:00 -0500

    fixes in profile picture

--- src/containers/profile-change-picture/ProfileChangePicture.jsx
+++ src/containers/profile-change-picture/ProfileChangePicture.jsx
@@ -35,6 +35,7 @@ const ProfileChangePicture = () => {
 	const hiddenFileInput = useRef(null);
 	const iconEditorRef = useRef(null);
 	const dispatch = useDispatch();
+
 	const { onClose: handleClose, onExiting: handleExiting } = useModal({
 		closeCallback: () => dispatch(onHideEditProfilePicture()),
 	});
@@ -49,12 +50,20 @@ const ProfileChangePicture = () => {
 		setAvatar(fileUploaded);
 	};
 
-	const handleSaveProfilePicture = () =>
-		dispatch(onChangeProfilePicture(avatar));
+	const handleSaveProfilePicture = () => {
+		const img = iconEditorRef.current.getImageScaledToCanvas().toDataURL();
+		let imageFile;
+		fetch(img)
+			.then((res) => res.blob())
+			.then((blob) => {
+				imageFile = new File([blob], avatar.name);
+				dispatch(onChangeProfilePicture(imageFile));
+			});
+	};
 
 	useEffect(() => {
-		if (user.profile_image) setAvatar(user.profile_image.urls.medium);
-	}, [user.profile_image]);
+		if (user?.profile_image?.urls) setAvatar(user.profile_image.urls.medium);
+	}, [user.profile_image?.urls]);
 
 	return (
 		<ModalLayout
