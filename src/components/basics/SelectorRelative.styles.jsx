5f1ff910c9a7f990428234c26ba25b5d65f5fa84
Andy Francisco Olarte Cardona <40371687+andyOlarte514@users.noreply.github.com>
2021-10-24 19:20:38 -0500

    change
    change selector relative component to use in modal team

--- src/components/basics/SelectorRelative.styles.jsx
+++ src/components/basics/SelectorRelative.styles.jsx
@@ -1,64 +1,69 @@
 import styled from 'styled-components';
-import { COLOR_WHITE, SHADOW_GREY, COLOR_MINE_SHAFT, COLOR_LIGHTGREY } from '../../config/constants/styled-vars';
-
+import {
+	COLOR_WHITE,
+	SHADOW_GREY,
+	COLOR_MINE_SHAFT,
+	COLOR_LIGHTGREY,
+} from '../../config/constants/styled-vars';
 
 export const Container = styled.div`
-  position: relative;
-  width: ${({ width }) => width || '100%'};
+	position: relative;
+	width: ${({ width }) => width || '100%'};
 `;
 
 export const Content = styled.div`
-  max-height: ${({ maxHeight }) => maxHeight || '100%'};
-  margin-top: 8px;
-  position: absolute;
-  border: 1px solid #CCC;
-  background-color: ${COLOR_WHITE};
-  min-width: 160px;
-  min-height: 30px;
-  width: ${({ width }) => width || '100%'};
-  padding: 4px 4px;
-  z-index: 1;
-  overflow-y: auto;
-  display: ${({ isOpen }) => isOpen ? 'block' : 'none'};
-  ${({ isOpen }) => isOpen ? `box-shadow: ${SHADOW_GREY}` : ''};
-  ${({ right }) => right ? 'right: 0' : 'left: 0'};
+	max-height: ${({ maxHeight }) => maxHeight || '100%'};
+	margin-top: 8px;
+	position: absolute;
+	border: 1px solid #ccc;
+	background-color: ${COLOR_WHITE};
+	min-width: 160px;
+	min-height: 30px;
+	width: ${({ width }) => width || '100%'};
+	padding: 4px 4px;
+	z-index: 1;
+	overflow-y: auto;
+	display: ${({ isOpen }) => (isOpen ? 'block' : 'none')};
+	${({ isOpen }) => (isOpen ? `box-shadow: ${SHADOW_GREY}` : '')};
+	${({ right }) => (right ? 'right: 0' : 'left: 0')};
 `;
 
 export const Section = styled.div`
-  width: 100%;
-  cursor: pointer;
-  margin: auto 0;
-  display: flex;
-  align-items: center;
+	width: 100%;
+	cursor: pointer;
+	margin: auto 0;
+	display: flex;
+	align-items: center;
 `;
 
 export const Option = styled.section`
-  box-sizing: border-box;
-  color: ${COLOR_MINE_SHAFT};
-  cursor: pointer;
-  font-family: Lato;
-  font-size: 12px;
-  letter-spacing: 0.86px;
-  padding: 10px 23px;
-  width: 100%;
+	box-sizing: border-box;
+	color: ${COLOR_MINE_SHAFT};
+	cursor: pointer;
+	font-family: Lato;
+	font-size: 12px;
+	letter-spacing: 0.86px;
+	padding: 10px 23px;
+	width: 100%;
 
-  &:hover {
-    background-color: #EEE;
-  }
+	&:hover {
+		background-color: #eee;
+		${({ hoverPrimaryColor }) => hoverPrimaryColor && 'color: #3ac0ad'};
+	}
 
-  &:first-child {
-    margin: 6px 0 0;
-  }
+	&:first-child {
+		margin: 6px 0 0;
+	}
 
-  &:last-child {
-    margin: 0 0 6px;
-  }
+	&:last-child {
+		margin: 0 0 6px;
+	}
 `;
 
 export const NoOptions = styled.div`
-  font-family: Lato;
-  font-size: 12px;
-  letter-spacing: 0.86px;
-  padding: 10px 23px;
-  color: ${COLOR_LIGHTGREY};
-`
+	font-family: Lato;
+	font-size: 12px;
+	letter-spacing: 0.86px;
+	padding: 10px 23px;
+	color: ${COLOR_LIGHTGREY};
+`;
