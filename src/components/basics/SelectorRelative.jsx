87f03c50f85febe5e7c7551f456d11b91dd7a02a
Andy Francisco Olarte Cardona <40371687+andyOlarte514@users.noreply.github.com>
2021-10-24 19:20:38 -0500

    change
    change selector relative component to use in modal team

--- src/components/basics/SelectorRelative.jsx
+++ src/components/basics/SelectorRelative.jsx
@@ -1,77 +1,99 @@
 import React, { useEffect, useState, useRef } from 'react';
 import PropTypes from 'prop-types';
-import { Container, Content, Option, Section, NoOptions } from './SelectorRelative.styles';
+import {
+	Container,
+	Content,
+	Option,
+	Section,
+	NoOptions,
+} from './SelectorRelative.styles';
 
 const propTypes = {
-  options: PropTypes.arrayOf(PropTypes.shape({
-    id: PropTypes.oneOfType([
-      PropTypes.string,
-      PropTypes.number,
-    ]),
-    name: PropTypes.string,
-  })),
-  onChange: PropTypes.func,
-  right: PropTypes.bool,
+	options: PropTypes.arrayOf(
+		PropTypes.shape({
+			id: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+			name: PropTypes.string,
+		}),
+	),
+	onChange: PropTypes.func,
+	right: PropTypes.bool,
 };
 
 const defaultProps = {
-  options: [],
-  onChange: () => { },
-  right: false,
+	options: [],
+	onChange: () => {},
+	right: false,
 };
 
-const SelectorRelative = ({ options, onChange, renderInput, width, maxHeight, right }) => {
-  const [isOpen, setIsOpen] = useState(false);
-  const toggle = () => setIsOpen(!isOpen);
+const SelectorRelative = ({
+	options,
+	onChange,
+	renderInput,
+	width,
+	maxHeight,
+	right,
+	hoverPrimaryColor,
+}) => {
+	const [isOpen, setIsOpen] = useState(false);
+	const toggle = () => setIsOpen(!isOpen);
 
-  const onChangeOption = value => () => {
-    onChange(value);
-    toggle();
-  }
+	const onChangeOption = (value) => () => {
+		onChange(value);
+		toggle();
+	};
 
-  const onClickOusite = callback => {
-    const innerRef = useRef();
-    const callbackRef = useRef();
+	const onClickOusite = (callback) => {
+		const innerRef = useRef();
+		const callbackRef = useRef();
 
-    useEffect(() => {
-      callbackRef.current = callback;
-    });
+		useEffect(() => {
+			callbackRef.current = callback;
+		});
 
-    useEffect(() => {
-      const handleClick = e => {
-        if (
-          innerRef.current &&
-          callbackRef.current &&
-          !innerRef.current.contains(e.target)
-        ) {
-          callbackRef.current(e);
-        }
-      }
-      document.addEventListener("click", handleClick);
-      return () => document.removeEventListener("click", handleClick);
-    }, []);
-    return innerRef;
-  };
+		useEffect(() => {
+			const handleClick = (e) => {
+				if (
+					innerRef.current &&
+					callbackRef.current &&
+					!innerRef.current.contains(e.target)
+				) {
+					callbackRef.current(e);
+				}
+			};
+			document.addEventListener('click', handleClick);
+			return () => document.removeEventListener('click', handleClick);
+		}, []);
+		return innerRef;
+	};
 
-  const innerRef = onClickOusite(() => {
-    if (!isOpen) return
-    setIsOpen(false);
-  });
+	const innerRef = onClickOusite(() => {
+		if (!isOpen) return;
+		setIsOpen(false);
+	});
 
-
-  return (
-    <Container isOpen={isOpen} ref={innerRef} width={width}>
-      <Section onClick={toggle}>
-        {renderInput}
-      </Section>
-      <Content isOpen={isOpen} width={width} maxHeight={maxHeight} right={right}>
-        {options.map(option => (
-          <Option key={option.id} onClick={onChangeOption(option)} value={option.id}>{option.label}</Option>
-        ))}
-        {!options.length && <NoOptions>No hay Opciones Disponibles</NoOptions>}
-      </Content>
-    </Container>
-  );
+	return (
+		<Container isOpen={isOpen} ref={innerRef} width={width}>
+			<Section onClick={toggle}>{renderInput}</Section>
+			<Content
+				isOpen={isOpen}
+				width={width}
+				maxHeight={maxHeight}
+				right={right}
+			>
+				{options.map((option) => (
+					<Option
+						key={option.id}
+						onClick={onChangeOption(option)}
+						value={option.id}
+						hoverPrimaryColor={hoverPrimaryColor}
+					>
+						{option.label}
+					</Option>
+				))}
+				{!options.length && <NoOptions>No hay Opciones Disponibles</NoOptions>}
+			</Content>
+		</Container>
+	);
 };
 
 SelectorRelative.propTypes = propTypes;
