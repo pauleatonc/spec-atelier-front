20e09e619d9a8e534cc4ac25e827f1e9896caf57
Andy Francisco Olarte Cardona <40371687+andyOlarte514@users.noreply.github.com>
2021-10-24 19:51:17 -0500

    featute
    consume check_active_email enpodint

--- src/services/users.service.js
+++ src/services/users.service.js
@@ -3,9 +3,13 @@ import { getJsonRequest, postJsonRequest } from '../modules/requests';
 import { factoryService, formatParams } from '../modules/services';
 
 export const getUsers = factoryService((params) =>
-  getJsonRequest(`${API_BASE_URL}/users${formatParams(params || {})}`),
+	getJsonRequest(`${API_BASE_URL}/users${formatParams(params || {})}`),
 );
 
 export const impersonateUser = factoryService((data) =>
-  postJsonRequest(`${API_BASE_URL}/users/impersonate`, data),
+	postJsonRequest(`${API_BASE_URL}/users/impersonate`, data),
+);
+
+export const checkEmail = factoryService((data) =>
+	postJsonRequest(`${API_BASE_URL}/users/check_active_email`, data),
 );
