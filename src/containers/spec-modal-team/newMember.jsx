781b285922acd0ca9d44ea215e27ad4eab5be09f
Andy Francisco Olarte Cardona <40371687+andyOlarte514@users.noreply.github.com>
2021-11-17 21:53:25 -0500

    feature
    modal new member team

--- src/containers/spec-modal-team/newMember.jsx
+++ src/containers/spec-modal-team/newMember.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
+import { useParams } from 'react-router';
 
 import { Button } from '../../components/SpecComponents';
 import ModalLayout from '../../components/layouts/ModalLayout';
@@ -9,54 +10,114 @@ import closeSource from '../../assets/images/icons/close.svg';
 import dropArrowSource from '../../assets/images/icons/primary-arrow-down.svg';
 import { CloseIcon } from '../profile-change-picture/ProfileChangePicture.styles';
 import { VARIANTS_BUTTON } from '../../config/constants/button-variants';
-import { OPTIONS_PERMISION } from './constants';
+import { useTextarea, useInput } from '../../components/inputs/Inputs.hooks';
 
+import { OPTIONS_PERMISION, EMAIL_REGEX } from './constants';
 import ProjectInfoShare from './components/ProjectInfoShare';
-import { onHideModal, checkUserEmail } from './actions';
+import {
+	onHideModal,
+	checkUserEmail,
+	sendUserInvitation,
+	hideDisclaimer,
+} from './actions';
 import {
 	Container,
 	ButtonCloseContainer,
 	Title,
 	SearcherContainer,
-	DisclaimerUserNotSpec,
+	Disclaimer,
 	IconInfo,
 	DescDisclaimer,
 	EmailDesc,
 	Label,
+	InputMailContainer,
 	Searcher,
 	PermissionSelectorContainer,
 	PermisionLabel,
 	IconArrowDown,
+	TitleConfigContainer,
 	TitleConfig,
 	Message,
 	ContainerButtons,
+	ErrorInput,
 } from './styles';
 
 const SpecModalNewMember = () => {
 	const dispatch = useDispatch();
-	const { newMemberModal: show } = useSelector((state) => state.specModalTeam);
+	const { id: projectID } = useParams();
+	const [currentCheckMail, setCurrentCheckMail] = useState('');
+	const [inputMailInvalid, setInputMailInvalid] = useState(false);
 	const [permision, setPermision] = useState(OPTIONS_PERMISION[0]);
+	const [isAllProject, setIsAllProject] = useState(false);
+	const [selectedSections, setSelectedSections] = useState([]);
+	const [selectedItems, setSelectedItems] = useState([]);
+	const {
+		onChange: handleMailChange,
+		set: setMailValue,
+		value: mailValue,
+	} = useInput('');
+	const {
+		onChange: handleMessageChange,
+		set: setMessageValue,
+		value: messageValue,
+	} = useTextarea('');
+	const { newMemberModal: show, showDisclaimer } = useSelector(
+		(state) => state.specModalTeam,
+	);
 	const { onClose: handleClose, onExiting: handleExiting } = useModal({
 		closeCallback: () => dispatch(onHideModal()),
 		exitingCallback: () => {
-			console.log('exitingCallback in SpecModalNewMember');
+			setMessageValue('');
+			setMailValue('');
 		},
 	});
-	const handleBlur = ({ target: { value } }) => dispatch(checkUserEmail(value));
+
+	const handleBlur = ({ target: { value } }) => {
+		if (value) {
+			if (EMAIL_REGEX.test(value)) {
+				setInputMailInvalid(false);
+				setCurrentCheckMail(value);
+				dispatch(checkUserEmail(value));
+			} else {
+				setInputMailInvalid(true);
+			}
+		} else if (showDisclaimer) dispatch(hideDisclaimer());
+	};
+
+	const sendInvitation = () => {
+		const invitation = {
+			recipients: [mailValue],
+			sections: selectedSections,
+			items: selectedItems,
+			all: isAllProject,
+			ability: permision.value,
+			...(messageValue && { message: messageValue }),
+		};
+		dispatch(sendUserInvitation(projectID, invitation));
+	};
 
 	return (
 		<ModalLayout show={show} onClose={handleClose} onExiting={handleExiting}>
 			<Container>
 				<ButtonCloseContainer>
+					<Title>Invita a tu equipo al proyecto</Title>
 					<CloseIcon alt="Cerrar" src={closeSource} onClick={handleClose} />
 				</ButtonCloseContainer>
-				<Title>Invita a tu equipo al proyecto</Title>
+
 				<SearcherContainer>
 					<Label>Compartir con:</Label>
-					<Searcher
-						placeholder="Añade a un miembro del equipo"
-						onBlur={handleBlur}
-					/>
+					<InputMailContainer>
+						<Searcher
+							inputMailInvalid={inputMailInvalid}
+							placeholder="Añade a un miembro del equipo"
+							onBlur={handleBlur}
+							onChange={handleMailChange}
+							value={mailValue}
+						/>
+						{inputMailInvalid && (
+							<ErrorInput>Formato de email invalido</ErrorInput>
+						)}
+					</InputMailContainer>
 					<PermissionSelectorContainer>
 						<SelectorRelative
 							name="sort"
@@ -75,29 +136,52 @@ const SpecModalNewMember = () => {
 						/>
 					</PermissionSelectorContainer>
 				</SearcherContainer>
-				<DisclaimerUserNotSpec>
-					<IconInfo className="fas fa-info-circle" />
-					<DescDisclaimer>
-						<EmailDesc>carolina.perez@gmail.com</EmailDesc> no es parte de
-						SpecAtelier, se enviará un correo para unirse y tener acceso al
-						proyecto.
-					</DescDisclaimer>
-				</DisclaimerUserNotSpec>
-				<TitleConfig>¿Quién quieres compartir?</TitleConfig>
-				<ProjectInfoShare withChecks />
-				<Message placeholder="Añade un mensaje (opcional)" />
+				{showDisclaimer && (
+					<Disclaimer>
+						<IconInfo className="fas fa-info-circle" />
+						<DescDisclaimer>
+							<EmailDesc>{currentCheckMail}</EmailDesc> no es parte de
+							SpecAtelier, se enviará un correo para unirse y tener acceso al
+							proyecto.
+						</DescDisclaimer>
+					</Disclaimer>
+				)}
+				<TitleConfigContainer>
+					<TitleConfig>Partidas compartidas</TitleConfig>
+				</TitleConfigContainer>
+				<ProjectInfoShare
+					withChecks
+					isAllProject={isAllProject}
+					setIsAllProject={setIsAllProject}
+					selectedSections={selectedSections}
+					setSelectedSections={setSelectedSections}
+					selectedItems={selectedItems}
+					setSelectedItems={setSelectedItems}
+				/>
+				<Message
+					placeholder="Añade un mensaje (opcional)"
+					value={messageValue || ''}
+					onChange={handleMessageChange}
+				/>
 				<ContainerButtons>
 					<Button
 						variant={VARIANTS_BUTTON.CANCEL}
 						width="120px"
-						onClick={() => console.log('cancelar')}
+						onClick={handleClose}
 					>
 						Cancelar
 					</Button>
 					<Button
 						variant={VARIANTS_BUTTON.PRIMARY}
 						width="120px"
-						onClick={() => console.log('enviar')}
+						onClick={sendInvitation}
+						disabled={
+							!mailValue ||
+							(!isAllProject &&
+								!selectedSections.length &&
+								!selectedItems.length) ||
+							inputMailInvalid
+						}
 					>
 						Enviar
 					</Button>
