5bd0492ed2f60d79a36df764ed36847720b97c36
Andy Francisco Olarte Cardona <40371687+andyOlarte514@users.noreply.github.com>
2021-11-17 21:46:44 -0500

    fix
    use mail to get firt letter user if user name is null

--- src/components/IconUser/styles.jsx
+++ src/components/IconUser/styles.jsx
@@ -4,10 +4,7 @@ export const Container = styled.section`
 	align-items: center;
 	background-color: #757575;
 	border-radius: ${({ size = 24 }) => `${size / 2}px`};
-	color: #ffffff;
 	display: flex;
-	font-family: Lato;
-	font-size: ${({ fontSize = 12 }) => `${fontSize}px`};
 	justify-content: center;
 	height: ${({ size = 24 }) => `${size}px`};
 	width: ${({ size = 24 }) => `${size}px`};
@@ -22,4 +19,11 @@ export const Container = styled.section`
 export const ImageProfile = styled.img`
 	width: 100%;
 	height: 100%;
+	border-radius: ${({ size = 24 }) => `${size / 2}px`};
+`;
+
+export const LetterNameUser = styled.p`
+	color: #ffffff;
+	font-family: Lato;
+	font-size: ${({ fontSize = 12 }) => `${fontSize}px`};
 `;
