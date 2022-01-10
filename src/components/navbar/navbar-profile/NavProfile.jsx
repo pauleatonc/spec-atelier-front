91b483f61edb242785f399b17e82cb190b121a9d
Sophia-Sergio <san.storres@gmail.com>
2022-01-09 23:37:07 -0300

    Feat: close profile optiones when cick away

--- src/components/navbar/navbar-profile/NavProfile.jsx
+++ src/components/navbar/navbar-profile/NavProfile.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
+import ClickAwayListener from 'react-click-away-listener';
 import { logoutAction } from '../../../containers/auth/auth.actions';
 import {
 	ProfileButton,
@@ -16,19 +17,19 @@ import {
 } from './NavProfile.styles';
 
 const NavProfile = () => {
-	const [showOptions, setShowOtions] = useState(false);
+	const [showOptions, setShowOptions] = useState(false);
 	const { user } = useSelector((state) => state.profile);
-	const togglOptions = () => setShowOtions(!showOptions);
+	const toggleOptions = () => setShowOptions(!showOptions);
 	const dispatch = useDispatch();
 	const onLogout = () => dispatch(logoutAction());
 	return (
 		<>
-			<ProfileButton
-				type="button"
-				onClick={togglOptions}
-				onKeyPress={togglOptions}
-				isOpen={showOptions}
-			>
+      <ProfileButton
+        type="button"
+        onClick={toggleOptions}
+        onKeyPress={toggleOptions}
+        isOpen={showOptions}
+      >
 				<ProfilePictureContainer>
 					{user?.profile_image?.urls ? (
 						<ProfilePictureImage
@@ -40,42 +41,44 @@ const NavProfile = () => {
 					)}
 				</ProfilePictureContainer>
 			</ProfileButton>
-			<ProfileOptions show={showOptions}>
-				<OptionsContent>
-					{user.first_name && user.email && (
-						<>
-							<ProfileInfo>
-								<ProfilePictureContainer>
-									{user?.profile_image?.urls ? (
-										<ProfilePictureImage
-											src={user?.profile_image?.urls.original}
-											alt="profile icon"
-										/>
-									) : (
-										<i className="fas fa-user-circle" />
-									)}
-								</ProfilePictureContainer>
-								<ContainerNameUser>
-									<InfoUserName>{`${user.first_name} ${user.last_name}`}</InfoUserName>
-									<InfoUserName gray>{user.email}</InfoUserName>
-								</ContainerNameUser>
-							</ProfileInfo>
-							<Separator />
-						</>
-					)}
-					<Link to="/profile" style={{ textDecoration: 'none' }}>
-						<Option>Perfil</Option>
-					</Link>
-					<Separator />
-					{(user?.superadmin_role || user?.impersonated) && (
-						<Link to="/act-as-another-user" style={{ textDecoration: 'none' }}>
-							<Option>Suplantar Usuario</Option>
-						</Link>
-					)}
-					<Separator />
-					<Option onClick={onLogout}>Cerrar sesión</Option>
-				</OptionsContent>
-			</ProfileOptions>
+      <ClickAwayListener onClickAway={showOptions ? toggleOptions : () => {} }>
+        <ProfileOptions show={showOptions}>
+          <OptionsContent>
+            {user.first_name && user.email && (
+              <>
+                <ProfileInfo>
+                  <ProfilePictureContainer>
+                    {user?.profile_image?.urls ? (
+                      <ProfilePictureImage
+                        src={user?.profile_image?.urls.original}
+                        alt="profile icon"
+                      />
+                    ) : (
+                      <i className="fas fa-user-circle" />
+                    )}
+                  </ProfilePictureContainer>
+                  <ContainerNameUser>
+                    <InfoUserName>{`${user.first_name} ${user.last_name}`}</InfoUserName>
+                    <InfoUserName gray>{user.email}</InfoUserName>
+                  </ContainerNameUser>
+                </ProfileInfo>
+                <Separator />
+              </>
+            )}
+            <Link to="/profile" style={{ textDecoration: 'none' }}>
+              <Option>Perfil</Option>
+            </Link>
+            <Separator />
+            {(user?.superadmin_role || user?.impersonated) && (
+              <Link to="/act-as-another-user" style={{ textDecoration: 'none' }}>
+                <Option>Suplantar Usuario</Option>
+              </Link>
+            )}
+            <Separator />
+            <Option onClick={onLogout}>Cerrar sesión</Option>
+          </OptionsContent>
+        </ProfileOptions>
+      </ClickAwayListener>
 		</>
 	);
 };
