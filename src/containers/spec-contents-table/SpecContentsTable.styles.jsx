6815b2fc448653de9e143fb40c393ebe430a7f42
Andy Francisco Olarte Cardona <40371687+andyOlarte514@users.noreply.github.com>
2022-03-01 02:36:15 -0500

    styles
    format spaces

--- src/containers/spec-contents-table/SpecContentsTable.styles.jsx
+++ src/containers/spec-contents-table/SpecContentsTable.styles.jsx
@@ -1,164 +1,173 @@
 import styled from 'styled-components';
-import { ALTO, BLACK, CARIBBEAN_GREEN, CARIBBEAN_GREEN_OPACITY, JAVA, MERCURY, SILVER, WHITE } from '../../config/constants/styled-vars';
+import {
+  ALTO,
+  BLACK,
+  CARIBBEAN_GREEN,
+  CARIBBEAN_GREEN_OPACITY,
+  JAVA,
+  MERCURY,
+  SILVER,
+  WHITE,
+} from '../../config/constants/styled-vars';
 
 export const Root = styled.div`
-	box-sizing: border-box;
-	display: -webkit-box;
-	height: 100%;
-	position: relative;
-	width: 100%;
-	margin-bottom: 1%;
+  box-sizing: border-box;
+  display: -webkit-box;
+  height: 100%;
+  position: relative;
+  width: 100%;
+  margin-bottom: 1%;
 `;
 
 export const ContentTable = styled.div`
-	position: relative;
-	margin-left: auto;
-	margin-right: auto;
-	width: 970px;
-	height: 597px;
+  position: relative;
+  margin-left: auto;
+  margin-right: auto;
+  width: 970px;
+  height: 597px;
 `;
 
 export const Header = styled.td`
-	width: 100%;
-	height: 46px;
-	padding: 20px;
-	font-family: Lato;
-	font-size: 16px;
-	font-weight: bold;
-	font-stretch: normal;
-	font-style: normal;
-	line-height: 0.94;
-	letter-spacing: normal;
-	background-color: ${WHITE};
+  width: 100%;
+  height: 46px;
+  padding: 20px;
+  font-family: Lato;
+  font-size: 16px;
+  font-weight: bold;
+  font-stretch: normal;
+  font-style: normal;
+  line-height: 0.94;
+  letter-spacing: normal;
+  background-color: ${WHITE};
 `;
 
 export const ContentFooter = styled.div`
-	display: flex;
-	justify-content: space-between;
-	background-color: ${CARIBBEAN_GREEN_OPACITY};
-	padding: 20px;
+  display: flex;
+  justify-content: space-between;
+  background-color: ${CARIBBEAN_GREEN_OPACITY};
+  padding: 20px;
 `;
 
 export const TableElements = styled.h1`
-	font-family: Lato;
-	font-size: 14px;
-	font-weight: bold;
-	line-height: 1.07;
-	color: rgba(${BLACK}, 0.87);
+  font-family: Lato;
+  font-size: 14px;
+  font-weight: bold;
+  line-height: 1.07;
+  color: rgba(${BLACK}, 0.87);
 `;
 
 export const TableTotal = styled.h1`
-	height: 17px;
-	font-family: Lato;
-	font-size: 14px;
-	font-weight: bold;
-	line-height: 1.07;
-	color: rgba(${BLACK}, 0.87);
-	margin-right: ${({ mRight }) => `${mRight}px` || '0'};
+  height: 17px;
+  font-family: Lato;
+  font-size: 14px;
+  font-weight: bold;
+  line-height: 1.07;
+  color: rgba(${BLACK}, 0.87);
+  margin-right: ${({ mRight }) => `${mRight}px` || '0'};
 `;
 
 export const Title = styled.div`
-	font-family: Lato;
-	font-size: 16px;
-	font-weight: bold;
-	line-height: 0.94;
-	float: left;
+  font-family: Lato;
+  font-size: 16px;
+  font-weight: bold;
+  line-height: 0.94;
+  float: left;
 `;
 
 export const ButtonsHeader = styled.div`
-	font-family: Lato;
-	font-size: 16px;
-	font-weight: bold;
-	font-stretch: normal;
-	font-style: normal;
-	line-height: 0.94;
-	letter-spacing: normal;
-	display: flex;
-	float: right;
+  font-family: Lato;
+  font-size: 16px;
+  font-weight: bold;
+  font-stretch: normal;
+  font-style: normal;
+  line-height: 0.94;
+  letter-spacing: normal;
+  display: flex;
+  float: right;
 `;
 export const Button = styled.div`
-	cursor: pointer;
-	font-family: Lato;
-	font-size: 14px;
-	font-weight: bold;
-	font-stretch: normal;
-	font-style: normal;
-	line-height: normal;
-	letter-spacing: normal;
-	text-align: center;
-	color: ${CARIBBEAN_GREEN};
-	height: 20px;
-	display: flex;
+  cursor: pointer;
+  font-family: Lato;
+  font-size: 14px;
+  font-weight: bold;
+  font-stretch: normal;
+  font-style: normal;
+  line-height: normal;
+  letter-spacing: normal;
+  text-align: center;
+  color: ${CARIBBEAN_GREEN};
+  height: 20px;
+  display: flex;
 `;
 export const ImgButton = styled.img`
-	margin: 0px 5px 0px 39px;
+  margin: 0px 5px 0px 39px;
 `;
 
 export const ImgSubtotal = styled.img``;
 
 export const ImgExpanderAll = styled.img`
-	display: none;
+  display: none;
 `;
 
 export const Table = styled.table`
-	width: 100%;
-	border-radius: 2px;
-	border: solid 1px ${MERCURY};
-	background-color: ${WHITE};
+  width: 100%;
+  border-radius: 2px;
+  border: solid 1px ${MERCURY};
+  background-color: ${WHITE};
 `;
 export const TableThead = styled.thead`
-	padding: 8px;
-	height: 46px;
-	text-align: left;
-	border-radius: 2px;
-	border: solid 1px ${MERCURY};
+  padding: 8px;
+  height: 46px;
+  text-align: left;
+  border-radius: 2px;
+  border: solid 1px ${MERCURY};
 `;
 export const TableTbody = styled.tbody`
-	width: 2px;
-	text-align: left;
+  width: 2px;
+  text-align: left;
 `;
 
 export const TableTd = styled.td`
-	padding: ${({ isTypeUnity }) => (isTypeUnity ? '16.5px' : '20px')} 15px;
-	border-top: 1px solid ${ALTO};
-	border-bottom: 1px solid ${ALTO};
-	font-family: Lato;
-	font-size: 13px;
-	font-weight: ${({ isTypeUnity }) => (isTypeUnity ? 'normal' : 'bold')};
-	line-height: 0.92;
-	letter-spacing: normal;
-	color: rgba(${BLACK}, 0.87);
-	text-align: ${({ isDesc }) => (isDesc ? 'left' : 'center')};
+  padding: ${({ isTypeUnity }) => (isTypeUnity ? '16.5px' : '20px')} 15px;
+  border-top: 1px solid ${ALTO};
+  border-bottom: 1px solid ${ALTO};
+  font-family: Lato;
+  font-size: 13px;
+  font-weight: ${({ isTypeUnity }) => (isTypeUnity ? 'normal' : 'bold')};
+  line-height: 0.92;
+  letter-spacing: normal;
+  color: rgba(${BLACK}, 0.87);
+  text-align: ${({ isDesc }) => (isDesc ? 'left' : 'center')};
 `;
 
 export const TableTh = styled.th`
-	font-family: Lato;
-	font-size: 13px;
-	line-height: 1.25;
-	letter-spacing: normal;
-	color: rgba(${BLACK}, 0.54);
-	padding: 20px 15px;
-	text-align: ${({ isDesc }) => (isDesc ? 'left' : 'center')};
+  font-family: Lato;
+  font-size: 13px;
+  line-height: 1.25;
+  letter-spacing: normal;
+  color: rgba(${BLACK}, 0.54);
+  padding: 20px 15px;
+  text-align: ${({ isDesc }) => (isDesc ? 'left' : 'center')};
 `;
 
 export const TableInput = styled.input`
-	border-style: none;
-	width: 56px;
-	border-bottom: solid 1px ${SILVER};
+  border-style: none;
+  width: 56px;
+  border-bottom: solid 1px ${SILVER};
 `;
 
 export const ButtonConsult = styled.div`
-	font-family: Lato;
-	font-size: 14px;
-	font-weight: bold;
-	color: ${CARIBBEAN_GREEN};
-	cursor: pointer;
+  font-family: Lato;
+  font-size: 14px;
+  font-weight: bold;
+  color: ${CARIBBEAN_GREEN};
+  cursor: pointer;
 `;
 
 export const IconExpan = styled.div`
-	color: ${JAVA};
+  color: ${JAVA};
 `;
 
 export const ContainerTotalTable = styled.div`
-	display: flex;
+  display: flex;
 `;
--- src/containers/spec-modal-team/components/ProjectInfoShare/index.jsx
+++ src/containers/spec-modal-team/components/ProjectInfoShare/index.jsx
@@ -3,96 +3,96 @@ import React, { useEffect } from 'react';
 
 import NestedChecklist from './components/NestedCheckList';
 import {
-	Container,
-	SelectAllProject,
-	AllProjectLabel,
-	IconCheck,
+  Container,
+  SelectAllProject,
+  AllProjectLabel,
+  IconCheck,
 } from './styles';
 
 const ProjectInfoShare = ({ withChecks, checklistData, setChecklistData }) => {
-	const getFlattedChildren = (node) => {
-		let flat = [node];
-		node.children.forEach((child) => {
-			flat = [...flat, ...getFlattedChildren(child)];
-		});
-		return flat;
-	};
+  const getFlattedChildren = (node) => {
+    let flat = [node];
+    node.children.forEach((child) => {
+      flat = [...flat, ...getFlattedChildren(child)];
+    });
+    return flat;
+  };
 
-	const isAllSelected = (node) => {
-		let allSelected = true;
-		const flat = getFlattedChildren(node);
-		flat.forEach((child) => {
-			if (!child.isSelected) allSelected = false;
-		});
-		return allSelected;
-	};
+  const isAllSelected = (node) => {
+    let allSelected = true;
+    const flat = getFlattedChildren(node);
+    flat.forEach((child) => {
+      if (!child.isSelected) allSelected = false;
+    });
+    return allSelected;
+  };
 
-	const allSelected = isAllSelected(checklistData);
+  const allSelected = isAllSelected(checklistData);
 
-	const setAllChildren = (node, value) => {
-		const flat = getFlattedChildren(node);
-		flat.forEach((child) => {
-			child.isSelected = value;
-		});
-	};
+  const setAllChildren = (node, value) => {
+    const flat = getFlattedChildren(node);
+    flat.forEach((child) => {
+      child.isSelected = value;
+    });
+  };
 
-	const traverseAndToggleNodeChildren = (data, value) => {
-		let toggled = false;
-		data.forEach((node) => {
-			if (node.value === value) {
-				toggled = true;
-				let newValue = true;
-				if (isAllSelected(node)) newValue = false;
-				node.isSelected = newValue;
-				setAllChildren(node, newValue);
-			}
-		});
+  const traverseAndToggleNodeChildren = (data, value) => {
+    let toggled = false;
+    data.forEach((node) => {
+      if (node.value === value) {
+        toggled = true;
+        let newValue = true;
+        if (isAllSelected(node)) newValue = false;
+        node.isSelected = newValue;
+        setAllChildren(node, newValue);
+      }
+    });
 
-		if (!toggled) {
-			for (let i = 0; i < data.length; i += 1) {
-				const node = data[i];
-				const {
-					data: newChildren,
-					toggled: toggledTwo,
-				} = traverseAndToggleNodeChildren(node.children, value);
-				node.children = newChildren;
-				if (toggledTwo) break;
-			}
-		}
+    if (!toggled) {
+      for (let i = 0; i < data.length; i += 1) {
+        const node = data[i];
+        const {
+          data: newChildren,
+          toggled: toggledTwo,
+        } = traverseAndToggleNodeChildren(node.children, value);
+        node.children = newChildren;
+        if (toggledTwo) break;
+      }
+    }
 
-		return { data, toggled };
-	};
+    return { data, toggled };
+  };
 
-	const toggleAllChildren = (value) => {
-		setChecklistData((data) => {
-			const { data: newChildren } = traverseAndToggleNodeChildren(
-				data.children,
-				value,
-			);
-			const newData = { ...data, children: newChildren };
-			return newData;
-		});
-	};
+  const toggleAllChildren = (value) => {
+    setChecklistData((data) => {
+      const { data: newChildren } = traverseAndToggleNodeChildren(
+        data.children,
+        value,
+      );
+      const newData = { ...data, children: newChildren };
+      return newData;
+    });
+  };
 
-	const toggleAllProject = () => {
-		setChecklistData((data) => {
-			const flat = getFlattedChildren(data);
-			flat.forEach((node) => {
-				node.isSelected = !allSelected;
-			});
-			return { ...data };
-		});
-	};
+  const toggleAllProject = () => {
+    setChecklistData((data) => {
+      const flat = getFlattedChildren(data);
+      flat.forEach((node) => {
+        node.isSelected = !allSelected;
+      });
+      return { ...data };
+    });
+  };
 
-	useEffect(() => {
-		const nodes = getFlattedChildren(checklistData);
-		const selected = [];
-		nodes.forEach((node) => {
-			if (node.isSelected) selected.push(node.value);
-		});
-	}, [checklistData]);
+  useEffect(() => {
+    const nodes = getFlattedChildren(checklistData);
+    const selected = [];
+    nodes.forEach((node) => {
+      if (node.isSelected) selected.push(node.value);
+    });
+  }, [checklistData]);
 
-	return (
+  return (
     <Container>
       {withChecks && (
         <SelectAllProject onClick={toggleAllProject}>
