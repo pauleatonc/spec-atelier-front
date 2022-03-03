bad3021a1ae23c755609cd35c119def04ebcff89
Andy Francisco Olarte Cardona <40371687+andyOlarte514@users.noreply.github.com>
2022-03-03 13:37:49 -0500

    styles
    format spaces

--- src/containers/routes/PublicRoute.jsx
+++ src/containers/routes/PublicRoute.jsx
@@ -1,28 +1,35 @@
-import React, {useEffect} from 'react';
+import React, { useEffect } from 'react';
 import { Route, Redirect } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
 import { getNotifications } from '../spec-header/SpecHeader.actions';
 
 const PublicRoute = ({ component: Component, restricted, ...rest }) => {
   const dispatch = useDispatch();
-  const { isLogin } = useSelector(state => state.auth);
+  const { isLogin } = useSelector((state) => state.auth);
   const { actionGet } = useSelector((state) => state.specHeader);
   useEffect(() => {
-		const interval = setInterval(() => {
-      if(isLogin){
-        if(actionGet){
+    const interval = setInterval(() => {
+      if (isLogin) {
+        if (actionGet) {
           dispatch(getNotifications());
         }
       }
-	}, 30000);
-		return () => clearInterval(interval);
-	}, [actionGet]);
+    }, 30000);
+    return () => clearInterval(interval);
+  }, [actionGet]);
   return (
     // restricted = false meaning public route
     // restricted = true meaning restricted (not public) route
-    <Route {...rest} render={props => (
-      isLogin && restricted ? <Redirect to="/projects" /> : <Component {...props} />
-    )} />
+    <Route
+      {...rest}
+      render={(props) =>
+        isLogin && restricted ? (
+          <Redirect to="/projects" />
+        ) : (
+          <Component {...props} />
+        )
+      }
+    />
   );
 };
 
