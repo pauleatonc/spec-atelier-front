4c203baf20f967883ea99d4fa95f606824140860
Andy Francisco Olarte Cardona <40371687+andyOlarte514@users.noreply.github.com>
2022-05-25 16:58:39 -0500

    fix
    hide Gestion de cambios button if user isn't ower project

--- src/containers/spec-history/components/ButtonsHistoryChangesManagement.jsx
+++ src/containers/spec-history/components/ButtonsHistoryChangesManagement.jsx
@@ -16,21 +16,30 @@ const ButtonsHistoryChangesManagement = () => {
   const { option_changes_management } = useSelector(
     (state) => state.specHistory,
   );
+  const {
+    project: { user_owner },
+  } = useSelector((state) => state.specDocument);
   const dispatch = useDispatch();
   const handleShowTable = (option) => () =>
     dispatch(changeOptionHistory(option));
 
   return (
     <HistoryChangeManagementContent>
-      <Item
-        active={option_changes_management === SPEC_CHANGE_MANAGEMENT}
-        onClick={handleShowTable(SPEC_CHANGE_MANAGEMENT)}
-      >
-        <ItemText active={option_changes_management === SPEC_CHANGE_MANAGEMENT}>
-          Gestión de cambios
-        </ItemText>
-      </Item>
-      <Separator />
+      {user_owner && (
+        <>
+          <Item
+            active={option_changes_management === SPEC_CHANGE_MANAGEMENT}
+            onClick={handleShowTable(SPEC_CHANGE_MANAGEMENT)}
+          >
+            <ItemText
+              active={option_changes_management === SPEC_CHANGE_MANAGEMENT}
+            >
+              Gestión de cambios
+            </ItemText>
+          </Item>
+          <Separator />
+        </>
+      )}
       <Item
         active={option_changes_management === SPEC_HISTORY_TABLE}
         onClick={handleShowTable(SPEC_HISTORY_TABLE)}
