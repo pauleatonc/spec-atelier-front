0ce6edd2c781062185f256ddf2be8523fdc7299f
Andy Francisco Olarte Cardona <40371687+andyOlarte514@users.noreply.github.com>
2022-07-20 21:38:10 -0500

    fix slections

--- src/containers/spec-modal-team/utils.js
+++ src/containers/spec-modal-team/utils.js
@@ -3,15 +3,17 @@ export const getCheckListData = (sections, permission) => ({
   value: 'root',
   isSelected: permission ? permission?.all : false,
   children: sections.map((section) => ({
+    id: section.id,
     label: section.name,
-    value: section.id,
+    value: `${section.id}-${section.name}`,
     users: section.users,
     isSelected: permission?.sections.length
       ? permission.sections.some((sec) => sec.id === section.id)
       : false,
     children: section.children.map((item) => ({
+      id: item.id,
       label: item.name,
-      value: item.id,
+      value: `${item.id}-${item.name}`,
       users: item.users,
       isSelected: permission?.sections.length
         ? permission.sections
@@ -28,16 +30,16 @@ export const getDataForService = (data) => {
   const selectedSections = [];
   const selectedItems = [];
   data.children.forEach(
-    (section) => section.isSelected && selectedSections.push(section.value),
+    (section) => section.isSelected && selectedSections.push(section.id),
   );
   data.children.forEach((section) => {
     const isSelectedSection = selectedSections.find(
-      (sec) => sec === section.value,
+      (sec) => sec === section.id,
     );
     section.children.forEach((item) => {
       if (item.isSelected) {
-        selectedItems.push(item.value);
-        if (!isSelectedSection) selectedSections.push(section.value);
+        selectedItems.push(item.id);
+        if (!isSelectedSection) selectedSections.push(section.id);
       }
     });
   });
